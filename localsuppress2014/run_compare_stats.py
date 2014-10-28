fe483534521b3077b8de0222df85fa146614bfee
boywert <boywert@Chaichalits-MacBook-Pro.local>
2014-10-28 12:13:35 +0000

    test

--- localsuppress2014/run_compare_stats.py
+++ localsuppress2014/run_compare_stats.py
@@ -1,6 +1,14 @@
 import os
 zlist = {}
 zlist[0] = "6.00"
+zlist[1] = "9.94"
+zlist[2] = "9.46"
+zlist[3] = "9.03"
+zlist[4] = "8.51"
+zlist[5] = "7.96"
+zlist[6] = "7.48"
+zlist[7] = "6.98"
+zlist[8] = "6.48"
 
 for i in range(len(zlist)):
     z = zlist[i]
