2c39599c35431926f643079d00d1c702e0e3b8fe
boywert <boywert@Chaichalits-MacBook-Pro.local>
2014-10-28 06:27:10 +0000

    test

--- localsuppress2014/compare_stats.py
+++ localsuppress2014/compare_stats.py
@@ -4,7 +4,8 @@ import pylab
 import sys
 import os
 import matplotlib.pyplot as plt
-LGalaxyStruct = __import__("dummy_dtype")
+import dummy_dtype as LGalaxyStruct
+
 sys.path.append("../python/")
 
 import read_lgal_advance as read_lgal
