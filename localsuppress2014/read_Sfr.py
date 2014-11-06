317895229df5d9cb08c25c8cce7e09feb99cfdf0
boywert <boywert@Chaichalits-MacBook-Pro.local>
2014-11-06 05:08:36 +0000

    fix

--- localsuppress2014/read_Sfr.py
+++ localsuppress2014/read_Sfr.py
@@ -13,7 +13,7 @@ folder = "sfr/"
 model_names = ["okamoto","noreionization","patchy_I"]
 model_labels = ["Okamoto et al. (2008)","No Reionization","Patchy Reionization (Gradual)"]
 color = ["r","g","b"]
-pattern = ["-","--","."]
+pattern = ["-","--","-."]
 sfr_t0 = {}
 sfr_t1 = {}
 sfr_t2 = {}
@@ -43,7 +43,8 @@ for i in range(len(model_names)):
     ax.plot(zlist,sfr_t0[index],color[i]+pattern[0],label=model_labels[i]+",Type 0")
     ax.plot(zlist,sfr_t1[index],color[i]+pattern[1],label=model_labels[i]+",Type 1")
     ax.plot(zlist,sfr_t2[index],color[i]+pattern[2],label=model_labels[i]+",Type 2")
-
+leg = ax.legend(loc='best', handlelength = 10,ncol=1, fancybox=True, prop={'size':10})
+leg.get_frame().set_linewidth(0)
 ax.set_yscale("log")
 fig.suptitle("SFR")
 fig.savefig("SFR_history.pdf",bbox_inches='tight')
