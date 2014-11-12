28cb5d1b97751d47e43d89a626f8c3cbabfcc2a6
boywert <boywert@Chaichalits-MacBook-Pro.local>
2014-11-12 16:23:31 +0000

    test

--- localsuppress2014/compare_stats.py
+++ localsuppress2014/compare_stats.py
@@ -30,6 +30,7 @@ def loadfilter(structfile):
     filter['sfh_DiskMass'] = True
     filter['GasDiskRadius'] = True
     filter['CoolingRate'] = True
+    filter['EjectedGas'] = True
     dt = LGalaxyStruct.struct_dtype
     return (filter,dt)
 
