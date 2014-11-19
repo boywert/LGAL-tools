979e163ff5363559dc9b86d279c479bb17349265
boywert <boywert@Chaichalits-MacBook-Pro.local>
2014-11-19 08:08:29 +0000

    gal vs galtree

---
+++ localsuppress2014/test_normal_vs_galtree.py
@@ -0,0 +1,52 @@
+from mass_fn import *
+import matplotlib
+matplotlib.use('Agg') 
+import pylab
+import sys
+import os
+import matplotlib.pyplot as plt
+os.system("cp dummy_dtype.py LGalaxyStruct.py")
+import LGalaxyStruct
+
+sys.path.append("../python/")
+import read_lgal_advance as read_lgal
+
+def loadfilter(structfile):
+    sys.path.insert(0,"../tmp/")
+    os.system("cp "+structfile+" ../tmp/LGalaxyStruct.py")
+    os.system("rm -f ../tmp/LGalaxyStruct.pyc")
+    reload(LGalaxyStruct)
+    filter = LGalaxyStruct.properties_used
+    for index in filter.keys():
+        filter[index] = True
+
+    filter['Type'] = True
+    filter['HaloIndex'] = True
+    filter['Sfr'] = True
+    filter['DiskMass'] = True
+    filter['BulgeMass'] = True
+    filter['ICM'] = True
+    filter['HotGas'] = True
+    filter['ColdGas'] = True
+    filter['sfh_numbins'] = True
+    filter['sfh_BulgeMass'] = True
+    filter['sfh_DiskMass'] = True
+    filter['GasDiskRadius'] = True
+    filter['CoolingRate'] = True
+    filter['EjectedMass'] = True
+    dt = LGalaxyStruct.struct_dtype
+    return (filter,dt)
+
+firstfile = 10
+lastfile = 10
+# read galtree
+
+(f,t) = loadfilter("/mnt/lustre/scratch/cs390/47Mpc/couple/model_002/fullgaltree/43000.00/LGalaxyStruct.py")
+(nGals_gt,galtree) = read_lgal.read_lgaltree_advance("/mnt/lustre/scratch/cs390/47Mpc/couple/model_002/fullgaltree/43000.00/","SA_",firstfile,lastfile,f,t,0)
+
+(f,t) = loadfilter("/mnt/lustre/scratch/cs390/47Mpc/couple/model_002/sams/43000.00/LGalaxyStruct.py")
+(nTrees_g,nGals_g,nTreeGals_g,gal) = read_lgal.readsnap_lgal_advance("/mnt/lustre/scratch/cs390/47Mpc/couple/model_002/sams/43000.00/","SA_z6.00",firstfile,lastfile,f,t,0)
+
+
+
+
