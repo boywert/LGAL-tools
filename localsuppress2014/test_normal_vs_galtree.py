5d22fbb885bbfc25730806984be9ee5d583b789f
boywert <boywert@Chaichalits-MacBook-Pro.local>
2014-11-19 23:36:52 +0000

    test

--- localsuppress2014/test_normal_vs_galtree.py
+++ localsuppress2014/test_normal_vs_galtree.py
@@ -52,7 +52,6 @@ for z in zlist:
     gal1 = galtree[numpy.where(galtree["SnapNum"] == i)[0]]
     (f2,t2) = loadfilter("/mnt/lustre/scratch/cs390/47Mpc/couple/model_002/sams/43000.00/LGalaxyStruct.py")
     (nTrees_g,nGals_g,nTreeGals_g,gal2) = read_lgal.readsnap_lgal_advance("/mnt/lustre/scratch/cs390/47Mpc/couple/model_002/sams/43000.00/","SA_z"+z,firstfile,lastfile,f2,t2,0)
-
     (f3,t3) = loadfilter("/mnt/lustre/scratch/cs390/47Mpc/outputs/okamoto/inputs/LGalaxyStruct.py")
     (nTrees_g,nGals_g,nTreeGals_g,gal3) = read_lgal.readsnap_lgal_advance("/mnt/lustre/scratch/cs390/47Mpc/outputs/okamoto/","SA_z"+z,firstfile,lastfile,f3,t3,0)
     
@@ -60,4 +59,5 @@ for z in zlist:
     (nTrees_g,nGals_g,nTreeGals_g,gal4) = read_lgal.readsnap_lgal_advance("/mnt/lustre/scratch/cs390/47Mpc/outputs/no_reionization/","SA_z"+z,firstfile,lastfile,f4,t4,0)
     i += 1
     print numpy.sum(gal1["Sfr"],dtype=numpy.float64),numpy.sum(gal2["Sfr"],dtype=numpy.float64),numpy.sum(gal3["Sfr"],dtype=numpy.float64),numpy.sum(gal4["Sfr"],dtype=numpy.float64)
+    print len(gal1),len(gal2),len(gal3),len(gal4)
     
