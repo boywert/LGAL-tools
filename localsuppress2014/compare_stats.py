d6ea418cffdacfb9824abd575cb19a4f3a2340d7
boywert <boywert@Chaichalits-MacBook-Pro.local>
2014-11-20 13:08:06 +0000

    test

--- localsuppress2014/compare_stats.py
+++ localsuppress2014/compare_stats.py
@@ -46,7 +46,7 @@ firstfile = 100
 lastfile = 100
 config = {}
 model_names = ["okamoto","noreionization","patchy_I"]
-struct_file = ["/mnt/lustre/scratch/cs390/codes/47Mpc/L-Galaxies_development/gal/LGalaxyStruct.py","/mnt/lustre/scratch/cs390/codes/47Mpc/L-Galaxies_development/gal/LGalaxyStruct.py","/mnt/lustre/scratch/cs390/47Mpc/couple/model_002/sams/43000.00/LGalaxyStruct.py"]
+struct_file = ["/mnt/lustre/scratch/cs390/codes/47Mpc/L-Galaxies_development/galnoxfrac/LGalaxyStruct.py","/mnt/lustre/scratch/cs390/codes/47Mpc/L-Galaxies_development/galnoxfrac/LGalaxyStruct.py","/mnt/lustre/scratch/cs390/47Mpc/couple/model_002/sams/43000.00/LGalaxyStruct.py"]
 
 dt = []
 filter = []
