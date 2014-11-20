c8559ab0fe0a3cd5523a7fe3e384c755d5cc3e95
boywert <boywert@Chaichalits-MacBook-Pro.local>
2014-11-20 05:10:06 +0000

    `test

--- localsuppress2014/test_normal_vs_galtree.py
+++ localsuppress2014/test_normal_vs_galtree.py
@@ -43,15 +43,15 @@ lastfile = 1
 filelist = "/mnt/lustre/scratch/cs390/47Mpc/snap_z.txt"
 zf = open(filelist,"r")
 zlist = zf.readlines()
-(f1,t1) = loadfilter("/mnt/lustre/scratch/cs390/codes/47Mpc/L-Galaxies_development/galtree/LGalaxyStruct.py")
-(nGals_gt,galtree) = read_lgal.read_lgaltree_advance("/mnt/lustre/scratch/cs390/47Mpc/outputs/no_reionization/","SA_",firstfile,lastfile,f1,t1,0)
+(f1,t1) = loadfilter("/mnt/lustre/scratch/cs390/47Mpc/couple/model_002/fullgaltree/43000.00/LGalaxyStruct.py")
+(nGals_gt,galtree) = read_lgal.read_lgaltree_advance("/mnt/lustre/scratch/cs390/47Mpc/couple/model_002/fullgaltree/43000.00/","SA_",firstfile,lastfile,f1,t1,0)
 i = 0
 for z in zlist:
     z = z.strip()
     print z
     gal1 = galtree[numpy.where(galtree["SnapNum"] == i)[0]]
-    (f2,t2) = loadfilter("/mnt/lustre/scratch/cs390/codes/47Mpc/L-Galaxies_development/gal/LGalaxyStruct.py")
-    (nTrees_g,nGals_g,nTreeGals_g,gal2) = read_lgal.readsnap_lgal_advance("/mnt/lustre/scratch/cs390/47Mpc/outputs/no_reionization/","SA_z"+z,firstfile,lastfile,f2,t2,0)
+    (f2,t2) = loadfilter("/mnt/lustre/scratch/cs390/47Mpc/couple/model_002/sams/43000.00/all/LGalaxyStruct.py")
+    (nTrees_g,nGals_g,nTreeGals_g,gal2) = read_lgal.readsnap_lgal_advance("/mnt/lustre/scratch/cs390/47Mpc/couple/model_002/sams/43000.00/all/","SA_z"+z,firstfile,lastfile,f2,t2,0)
     print gal1["Sfr"][numpy.where(gal1["Sfr"] > 100.)[0]]
     # (f3,t3) = loadfilter("/mnt/lustre/scratch/cs390/47Mpc/outputs/okamoto/inputs/LGalaxyStruct.py")
     # (nTrees_g,nGals_g,nTreeGals_g,gal3) = read_lgal.readsnap_lgal_advance("/mnt/lustre/scratch/cs390/47Mpc/outputs/okamoto/","SA_z"+z,firstfile,lastfile,f3,t3,0)
