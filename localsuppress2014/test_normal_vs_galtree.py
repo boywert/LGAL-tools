0965f937bceea3b4851f4105155e1a19c81c2aea
boywert <boywert@Chaichalits-MacBook-Pro.local>
2014-11-20 04:27:21 +0000

    test

--- localsuppress2014/test_normal_vs_galtree.py
+++ localsuppress2014/test_normal_vs_galtree.py
@@ -52,8 +52,7 @@ for z in zlist:
     gal1 = galtree[numpy.where(galtree["SnapNum"] == i)[0]]
     (f2,t2) = loadfilter("/mnt/lustre/scratch/cs390/codes/47Mpc/L-Galaxies_development/gal/LGalaxyStruct.py")
     (nTrees_g,nGals_g,nTreeGals_g,gal2) = read_lgal.readsnap_lgal_advance("/mnt/lustre/scratch/cs390/47Mpc/outputs/no_reionization/","SA_z"+z,firstfile,lastfile,f2,t2,0)
-    print t1,t2
-    #print gal1["Sfr"][numpy.where(gal1["Sfr"] > 100.)[0]]
+    print gal1["Sfr"][numpy.where(gal1["Sfr"] > 100.)[0]]
     # (f3,t3) = loadfilter("/mnt/lustre/scratch/cs390/47Mpc/outputs/okamoto/inputs/LGalaxyStruct.py")
     # (nTrees_g,nGals_g,nTreeGals_g,gal3) = read_lgal.readsnap_lgal_advance("/mnt/lustre/scratch/cs390/47Mpc/outputs/okamoto/","SA_z"+z,firstfile,lastfile,f3,t3,0)
     
