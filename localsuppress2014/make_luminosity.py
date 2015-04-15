716a4f45c869e28888aab708286c0ab2f0b0042b
Chaichalit Srisawat <boywert@gmail.com>
2015-04-15 08:11:38 +0100

    quick commit

--- localsuppress2014/make_luminosity.py
+++ localsuppress2014/make_luminosity.py
@@ -50,45 +50,122 @@ model_plot_patterns = ['r--','g--','b--']
 pylab.rc('text', usetex=True)
 zlistfile = "/mnt/lustre/scratch/cs390/47Mpc/snap_z.txt"
 zlist = open(zlistfile,"r").readlines()
-#z = zlist[int(sys.argv[1])].strip()
 
+def plot_uv_z6():
+    z = "6.06"
+    file_prefix = "SA_z"+z
+    firstfile = 0
+    lastfile = 127
+    config = {}
 
+    try:
+        gal
+    except NameError:
+        gal = {}
+        nTrees = {}
+        nGals = {}
+        nTreeGals = {}
 
-z = "6.98"
-file_prefix = "SA_z"+z
-firstfile = 0
-lastfile = 127
-config = {}
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    uv_luminosity.add_obs_uv_z7("../../codes/47Mpc/observed_UVL/",ax)
 
-try:
-    gal
-except NameError:
-    gal = {}
-    nTrees = {}
-    nGals = {}
-    nTreeGals = {}
+    for i in range(len(model_names)):
+        index = model_names[i]
+        if not index in gal:
+            (nTrees[index],nGals[index],nTreeGals[index],gal[index]) = read_lgal.readsnap_lgal_advance(model_paths[i],file_prefix,firstfile,lastfile,filter[i],dt[i],0)
 
-
-import uv_luminosity
-fig = plt.figure()
-ax = fig.add_subplot(111)
-uv_luminosity.add_obs_uv_z7("../../codes/47Mpc/observed_UVL/",ax)
-
-for i in range(len(model_names)):
-    index = model_names[i]
-    if not index in gal:
-        (nTrees[index],nGals[index],nTreeGals[index],gal[index]) = read_lgal.readsnap_lgal_advance(model_paths[i],file_prefix,firstfile,lastfile,filter[i],dt[i],0)
-
-    logf = -2.5*numpy.log10(gal[index]["Sfr"])
-    a = numpy.histogram(logf,bins=9,range=(-3.0,1.5))
-    x = a[1][0:len(a[1])-1]+0.25-18
-    y = a[0]/47.**3/0.5
-    ax.plot(x,y,model_plot_patterns[i],label=model_labels[i])
+        logf = -2.5*numpy.log10(gal[index]["Sfr"])
+        a = numpy.histogram(logf,bins=9,range=(-3.0,1.5))
+        x = a[1][0:len(a[1])-1]+0.25-18
+        y = a[0]/47.**3/0.5
+        ax.plot(x,y,model_plot_patterns[i],label=model_labels[i])
     
-leg = ax.legend(loc='best', handlelength = 10,ncol=1, fancybox=True, prop={'size':10})
-leg.get_frame().set_linewidth(0)
-ax.set_xlabel(r"M1600 - 5log(h)")
-ax.set_ylabel(r"numbers \mathrm{Mpc^{-3}} Mag$^-1$")
-ax.set_yscale("log")
-fig.savefig("uv_l_z7.pdf",bbox_inches='tight',pad_inches=0)
+    leg = ax.legend(loc='best', handlelength = 10,ncol=1, fancybox=True, prop={'size':10})
+    leg.get_frame().set_linewidth(0)
+    ax.set_xlabel(r"M1600 - 5log(h)")
+    ax.set_ylabel(r"numbers $\mathrm{Mpc^{-3} Mag^-1}$")
+    ax.set_yscale("log")
+    fig.savefig("uv_l_z6.pdf",bbox_inches='tight',pad_inches=0)
+
+
+
+def plot_uv_z8():
+    z = "7.96"
+    file_prefix = "SA_z"+z
+    firstfile = 0
+    lastfile = 127
+    config = {}
+
+    try:
+        gal
+    except NameError:
+        gal = {}
+        nTrees = {}
+        nGals = {}
+        nTreeGals = {}
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    uv_luminosity.add_obs_uv_z8("../../codes/47Mpc/observed_UVL/",ax)
+
+    for i in range(len(model_names)):
+        index = model_names[i]
+        if not index in gal:
+            (nTrees[index],nGals[index],nTreeGals[index],gal[index]) = read_lgal.readsnap_lgal_advance(model_paths[i],file_prefix,firstfile,lastfile,filter[i],dt[i],0)
+
+        logf = -2.5*numpy.log10(gal[index]["Sfr"])
+        a = numpy.histogram(logf,bins=9,range=(-3.0,1.5))
+        x = a[1][0:len(a[1])-1]+0.25-18
+        y = a[0]/47.**3/0.5
+        ax.plot(x,y,model_plot_patterns[i],label=model_labels[i])
+    
+    leg = ax.legend(loc='best', handlelength = 10,ncol=1, fancybox=True, prop={'size':10})
+    leg.get_frame().set_linewidth(0)
+    ax.set_xlabel(r"M1600 - 5log(h)")
+    ax.set_ylabel(r"numbers $\mathrm{Mpc^{-3} Mag^-1}$")
+    ax.set_yscale("log")
+    fig.savefig("uv_l_z8.pdf",bbox_inches='tight',pad_inches=0)
+
+
+
+def plot_uv_z7():
+    z = "6.98"
+    file_prefix = "SA_z"+z
+    firstfile = 0
+    lastfile = 127
+    config = {}
+
+    try:
+        gal
+    except NameError:
+        gal = {}
+        nTrees = {}
+        nGals = {}
+        nTreeGals = {}
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    uv_luminosity.add_obs_uv_z7("../../codes/47Mpc/observed_UVL/",ax)
+
+    for i in range(len(model_names)):
+        index = model_names[i]
+        if not index in gal:
+            (nTrees[index],nGals[index],nTreeGals[index],gal[index]) = read_lgal.readsnap_lgal_advance(model_paths[i],file_prefix,firstfile,lastfile,filter[i],dt[i],0)
+
+        logf = -2.5*numpy.log10(gal[index]["Sfr"])
+        a = numpy.histogram(logf,bins=9,range=(-3.0,1.5))
+        x = a[1][0:len(a[1])-1]+0.25-18
+        y = a[0]/47.**3/0.5
+        ax.plot(x,y,model_plot_patterns[i],label=model_labels[i])
+    
+    leg = ax.legend(loc='best', handlelength = 10,ncol=1, fancybox=True, prop={'size':10})
+    leg.get_frame().set_linewidth(0)
+    ax.set_xlabel(r"M1600 - 5log(h)")
+    ax.set_ylabel(r"numbers $\mathrm{Mpc^{-3} Mag^-1$")
+    ax.set_yscale("log")
+    fig.savefig("uv_l_z7.pdf",bbox_inches='tight',pad_inches=0)
+
+
+
 
