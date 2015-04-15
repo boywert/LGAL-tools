eb8532d1dddcc1d8fe37f5150893e02c4ed2854d
Chaichalit Srisawat <boywert@gmail.com>
2015-04-15 23:33:39 +0100

    quick commit

---
+++ localsuppress2014/make_powerspectrum.py
@@ -0,0 +1,49 @@
+import numpy
+import pylab
+from pylab import *
+from matplotlib import gridspec
+import power_spectrum
+
+class xfrac:
+    grid = 0
+    data = 0
+
+def read_xfrac(filename,doubleflag):
+    f = open(filename,"rb")
+    output = xfrac()
+    padd = numpy.fromfile(f,numpy.int32,1)[0]
+    output.grid = numpy.fromfile(f,numpy.int32,3)
+    padd = numpy.fromfile(f,numpy.int32,1)[0]
+    padd = numpy.fromfile(f,numpy.int32,1)[0]
+    if(doubleflag == 1):
+        output.data = numpy.fromfile(f,numpy.float64,output.grid[0]**3).reshape(( output.grid[0], output.grid[1], output.grid[2]))
+    else:
+        output.data = numpy.fromfile(f,numpy.float32,output.grid[0]**3).reshape(( output.grid[0], output.grid[1], output.grid[2]))
+    padd = numpy.fromfile(f,numpy.int32,1)[0]
+    return output
+
+
+
+def do_powerspectrum(filename,doubleflag):
+    xfrac = read_xfrac(filename,doubleflag).data
+    ps,bins = power_spectrum.power_spectrum_1d(xfrac, 47., kbins=153)
+    return ps
+
+def plot_powerspectrum(filelist,doubleflaglist,redshift):    
+    fig = pylab.figure()
+    ax = fig.add_subplot(111)
+    for i in range(filelist):
+        filename = filelist[ifile]+"/xfrac3d_"+redshift+".bin"
+        ps = do_powerspectrum(filename,doubleflaglist[ifile])
+        ax.plot(ps)
+        
+    fig.savefig(redshift+"_ps.pdf", bbox_inches='tight')
+    
+
+filelist = ["/mnt/lustre/scratch/cs390/codes/ionz_codes/nosupwithnohist/43000.00/","/mnt/lustre/scratch/cs390/codes/ionz_codes/okamotowithnohist/43000.00/","/mnt/lustre/scratch/cs390/47Mpc/couple/n306/xfrac/43000.00/"]
+doubleflaglist =[0,0,0]
+plot_powerspectrum(filelist,doubleflaglist,"9.938")
+plot_powerspectrum(filelist,doubleflaglist,"9.026")
+plot_powerspectrum(filelist,doubleflaglist,"7.960")
+plot_powerspectrum(filelist,doubleflaglist,"6.981")
+
--- localsuppress2014/plot_metallicity.py
+++ localsuppress2014/plot_metallicity.py
@@ -22,11 +22,9 @@ def loadfilter(structfile):
     for fi in filter:
         fi = False
     filter['Type'] = True
-    filter['Sfr'] = True
+    filter['Metallicity'] = True
     filter['HaloM_Crit200'] = True
-    filter['DiskMass'] = True
-    filter['BulgeMass'] = True
-    dt = LGalaxyStruct.struct_dtype
+     dt = LGalaxyStruct.struct_dtype
     return (filter,dt)
 
 
@@ -50,8 +48,8 @@ model_plot_patterns = ['r--','g--','b--']
 pylab.rc('text', usetex=True)
 zlistfile = "/mnt/lustre/scratch/cs390/47Mpc/snap_z.txt"
 zlist = open(zlistfile,"r").readlines()
-offset = 18.0
-def plot_uv_z6():
+
+def Metallicity_plot_z6():
     z = "6.06"
     file_prefix = "SA_z"+z
     firstfile = 0
@@ -68,85 +66,6 @@ def plot_uv_z6():
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    uv_luminosity.add_obs_uv_z6("../../codes/47Mpc/observed_UVL/",ax)
-
-    for i in range(len(model_names)):
-        index = model_names[i]
-        if not index in gal:
-            (nTrees[index],nGals[index],nTreeGals[index],gal[index]) = read_lgal.readsnap_lgal_advance(model_paths[i],file_prefix,firstfile,lastfile,filter[i],dt[i],0)
-        
-        logf = -2.5*numpy.log10(gal[index]["Sfr"])
-        a = numpy.histogram(logf,bins=9,range=(-3.0,1.5))
-        x = a[1][0:len(a[1])-1]+0.25-offset
-        y = a[0]/47.**3/0.5
-        ax.plot(x,y,model_plot_patterns[i],label=model_labels[i])
-    
-    leg = ax.legend(loc='best', handlelength = 10,ncol=1, fancybox=True, prop={'size':10})
-    leg.get_frame().set_linewidth(0)
-    ax.set_xlabel(r"M1600 - 5log(h)")
-    ax.set_ylabel(r"numbers $\mathrm{Mpc^{-3} Mag^-1}$")
-    ax.set_yscale("log")
-    fig.savefig("uv_l_z6.pdf",bbox_inches='tight',pad_inches=0)
-
-
-
-def plot_uv_z8():
-    z = "7.96"
-    file_prefix = "SA_z"+z
-    firstfile = 0
-    lastfile = 127
-    config = {}
-
-    try:
-        gal
-    except NameError:
-        gal = {}
-        nTrees = {}
-        nGals = {}
-        nTreeGals = {}
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    uv_luminosity.add_obs_uv_z8("../../codes/47Mpc/observed_UVL/",ax)
-
-    for i in range(len(model_names)):
-        index = model_names[i]
-        if not index in gal:
-            (nTrees[index],nGals[index],nTreeGals[index],gal[index]) = read_lgal.readsnap_lgal_advance(model_paths[i],file_prefix,firstfile,lastfile,filter[i],dt[i],0)
-
-        logf = -2.5*numpy.log10(gal[index]["Sfr"])
-        a = numpy.histogram(logf,bins=9,range=(-3.0,1.5))
-        x = a[1][0:len(a[1])-1]+0.25-offset
-        y = a[0]/47.**3/0.5
-        ax.plot(x,y,model_plot_patterns[i],label=model_labels[i])
-    
-    leg = ax.legend(loc='best', handlelength = 10,ncol=1, fancybox=True, prop={'size':10})
-    leg.get_frame().set_linewidth(0)
-    ax.set_xlabel(r"M1600 - 5log(h)")
-    ax.set_ylabel(r"numbers $\mathrm{Mpc^{-3} Mag^-1}$")
-    ax.set_yscale("log")
-    fig.savefig("uv_l_z8.pdf",bbox_inches='tight',pad_inches=0)
-
-
-
-def plot_uv_z7():
-    z = "6.98"
-    file_prefix = "SA_z"+z
-    firstfile = 0
-    lastfile = 127
-    config = {}
-
-    try:
-        gal
-    except NameError:
-        gal = {}
-        nTrees = {}
-        nGals = {}
-        nTreeGals = {}
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    uv_luminosity.add_obs_uv_z7("../../codes/47Mpc/observed_UVL/",ax)
 
     for i in range(len(model_names)):
         index = model_names[i]
