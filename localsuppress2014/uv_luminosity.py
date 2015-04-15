919267113b432ce14ab8f7110824f28583be57af
Chaichalit Srisawat <boywert@gmail.com>
2015-04-15 07:10:31 +0100

    quick commit

--- localsuppress2014/uv_luminosity.py
+++ localsuppress2014/uv_luminosity.py
@@ -11,7 +11,7 @@ def add_obs_uv_z8(observe_folder,ax):
 
     bouwens2011_errorup = (10.**(bouwens2011[:,1] + bouwens2011[:,5]) - 10.**bouwens2011[:,1])/hubble_h**3.
     bouwens2011_errordown = (10.**bouwens2011[:,1] - 10.**(bouwens2011[:,1] + bouwens2011[:,4]))/hubble_h**3.
-    ax.errorbar(bouwens2011_x,bouwens2011_y,yerr=bouwens2011_y/10., fmt='o',label="Bouwens et al. (2011)") 
+    ax.errorbar(bouwens2011_x,bouwens2011_y,yerr=[ bouwens2011_errordown, bouwens2011_errorup], fmt='o',label="Bouwens et al. (2011)") 
 
     mclure2010_file = observe_folder+"/mclure2010_z8.txt"
     mclure2010= numpy.loadtxt(mclure2010_file)
@@ -20,7 +20,7 @@ def add_obs_uv_z8(observe_folder,ax):
 
     mclure2010_errorup = (10.**(mclure2010[:,1] + mclure2010[:,3]) - 10.**mclure2010[:,1])/hubble_h**3.
     mclure2010_errordown = (10.**mclure2010[:,1] - 10.**(mclure2010[:,1] + mclure2010[:,2]))/hubble_h**3.
-    ax.errorbar(mclure2010_x,mclure2010_y,yerr=mclure2010_y/10., fmt='o',label="Mclure et al. (2011)") 
+    ax.errorbar(mclure2010_x,mclure2010_y,yerr=[mclure2010_errordown,mclure2010_errorup], fmt='o',label="Mclure et al. (2011)") 
   
     bouwens2010_file = observe_folder+"/bouwens2010_z8.txt"
     bouwens2010 = numpy.loadtxt(bouwens2011_file)
@@ -29,11 +29,31 @@ def add_obs_uv_z8(observe_folder,ax):
 
     bouwens2010_errorup = (10.**(bouwens2010[:,1] + bouwens2010[:,3]) - 10.**bouwens2010[:,1])/hubble_h**3.
     bouwens2010_errordown = (10.**bouwens2010[:,1] - 10.**(bouwens2010[:,1] + bouwens2010[:,2]))/hubble_h**3.
-    ax.errorbar(bouwens2010_x,bouwens2010_y,yerr=bouwens2010_y/10., fmt='o',label="Bouwens et al. (2010)")
-
-    
-
+    ax.errorbar(bouwens2010_x,bouwens2010_y,yerr=[bouwens2010_errordown,bouwens2010_errorup], fmt='o',label="Bouwens et al. (2010)")
     return ax
+
+def add_obs_uv_z7(observe_folder,ax):
+    hubble_h=0.7
+    mclure2010_file = observe_folder+"/mclure2010_z8.txt"
+    mclure2010= numpy.loadtxt(mclure2010_file)
+    mclure2010_x = mclure2010[:,0]-5.*numpy.log10(hubble_h)
+    mclure2010_y = (10.**mclure2010[:,1])/hubble_h**3.
+
+    mclure2010_errorup = (10.**(mclure2010[:,1] + mclure2010[:,3]) - 10.**mclure2010[:,1])/hubble_h**3.
+    mclure2010_errordown = (10.**mclure2010[:,1] - 10.**(mclure2010[:,1] + mclure2010[:,2]))/hubble_h**3.
+    ax.errorbar(mclure2010_x,mclure2010_y,yerr=[mclure2010_errordown,mclure2010_errorup], fmt='o',label="Mclure et al. (2011)") 
+  
+    oesch2010_file = observe_folder+"/oesch2010_z7.txt"
+    oesch2010 = numpy.loadtxt(oesch2010_file)
+    oesch2010_x = oesch2010[:,0]-5.*numpy.log10(hubble_h)
+    oesch2010_y = (10.**oesch2010[:,1])/hubble_h**3.
+
+    oesch2010_errorup = (10.**(oesch2010[:,1] + oesch2010[:,3]) - 10.**oesch2010[:,1])/hubble_h**3.
+    oesch2010_errordown = (10.**oesch2010[:,1] - 10.**(bouwens2010[:,1] + oesch2010[:,2]))/hubble_h**3.
+    ax.errorbar(oesch2010_x,oesch2010_y,yerr=[ oesch2010_errordown, oesch2010_errorup], fmt='o',label="Oesch et al. (2010)")
+    return ax
+  
+  
 
 def add_obs_uv_z6(observe_folder,ax):
     data_file = observe_folder+"bouwens2007_z6.txt"
@@ -43,7 +63,7 @@ def add_obs_uv_z6(observe_folder,ax):
 
     data_errorup = (10.**(data[:,1] + data[:,3]) - 10.**data[:,1])/hubble_h**3.
     data_errordown = (10.**data[:,1] - 10.**(data[:,1] + data[:,2]))/hubble_h**3.
-    ax.errorbar(data_x,data_y,yerr=data_y/10., fmt='o',label="Bouwens et al. (2007)") 
+    ax.errorbar(data_x,data_y,yerr=[data_errordown,data_errorup], fmt='o',label="Bouwens et al. (2007)") 
     return ax
 
 
