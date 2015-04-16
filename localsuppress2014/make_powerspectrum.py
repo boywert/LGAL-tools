5d2ee5af66eda87b6ddad4ad66515515c4980426
Chaichalit Srisawat <boywert@gmail.com>
2015-04-16 06:13:37 +0100

    quick commit

--- localsuppress2014/make_powerspectrum.py
+++ localsuppress2014/make_powerspectrum.py
@@ -36,14 +36,14 @@ def plot_powerspectrum(filelist,doubleflaglist,redshift):
         filename = filelist[ifile]+"/xfrac3d_"+redshift+".bin"
         ps = do_powerspectrum(filename,doubleflaglist[ifile])
         ax.plot(ps)
-        
+    ax.set_yscale('log')
     fig.savefig(redshift+"_ps.pdf", bbox_inches='tight')
     
 
 filelist = ["/mnt/lustre/scratch/cs390/codes/ionz_codes/nosupwithnohist/43000.00/","/mnt/lustre/scratch/cs390/codes/ionz_codes/okamotowithnohist/43000.00/","/mnt/lustre/scratch/cs390/47Mpc/couple/n306/xfrac/43000.00/"]
 doubleflaglist =[0,0,0]
-plot_powerspectrum(filelist,doubleflaglist,"9.938")
-plot_powerspectrum(filelist,doubleflaglist,"9.026")
-plot_powerspectrum(filelist,doubleflaglist,"7.960")
+# plot_powerspectrum(filelist,doubleflaglist,"9.938")
+# plot_powerspectrum(filelist,doubleflaglist,"9.026")
+# plot_powerspectrum(filelist,doubleflaglist,"7.960")
 plot_powerspectrum(filelist,doubleflaglist,"6.981")
 
