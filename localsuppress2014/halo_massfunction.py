f4fa67c1c0000affef990424e5294d9c3b413c4e
Chaichalit Srisawat <boywert@gmail.com>
2015-04-17 00:45:16 +0100

    quick commit

--- localsuppress2014/halo_massfunction.py
+++ localsuppress2014/halo_massfunction.py
@@ -21,7 +21,7 @@ def main():
     file_prefix = "trees_075."
     firstfile = 0
     lastfile = 127
-    (nTrees,nHalos,nTreeHalos,output_Halos) = read_lgal.read_lgal_input_tree(folder,file_prefix,firstfile,lastfile,filter,verbose=False)
+    (nTrees,nHalos,nTreeHalos,output_Halos) = read_lgal.read_lgal_input_tree(folder,file_prefix,firstfile,lastfile,filter,verbose=True)
     for i in range(len(zlist)):
         halos = output_Halos[numpy.where(output_Halos['SnapNum'] == i)]
         (massftn_x,massftn_y) = mass_fn.M200c_mass_fn(halos,mass_min=1e8,mass_max=1.e13,nbins=50)
