b60dd5d218f7d9d97e00a60ea382d536312f5192
Chaichalit Srisawat <boywert@gmail.com>
2015-04-17 01:52:04 +0100

    quick commit

--- localsuppress2014/halo_massfunction.py
+++ localsuppress2014/halo_massfunction.py
@@ -10,8 +10,8 @@ import mass_fn
 
 hubble_h = 0.7
 
-matplotlib.rc('font', family='sans-serif') 
-matplotlib.rc('font', serif='Helvetica Neue') 
+matplotlib.rc('font', family='serif') 
+matplotlib.rc('font', serif='Helvetica') 
 matplotlib.rc('text', usetex=True)
 
 zlistfile = "/mnt/lustre/scratch/cs390/47Mpc/snap_z.txt"
