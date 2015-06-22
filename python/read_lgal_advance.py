633bb359759ec3c0e1be8ef9365c30f8317c2aff
Chaichalit Srisawat <boywert@gmail.com>
2015-06-22 10:46:30 +0700

    quick commit

--- python/read_lgal_advance.py
+++ python/read_lgal_advance.py
@@ -4,25 +4,54 @@ import os
 import sys
 
 struct_lgalinput = numpy.dtype([
-('Descendant',numpy.int32,1),
-('FirstProgenitor',numpy.int32,1),
-('NextProgenitor',numpy.int32,1),
-('FirstHaloInFOFgroup',numpy.int32,1),
-('NextHaloInFOFgroup',numpy.int32,1),
-('Len',numpy.int32,1),
-('M_Mean200',numpy.float32,1),
-('M_Crit200',numpy.float32,1),
-('M_TopHat',numpy.float32,1),
-('Pos',numpy.float32,3),
-('Vel',numpy.float32,3),
-('VelDisp',numpy.float32,1),
-('Vmax',numpy.float32,1),
-('Spin',numpy.float32,3),
-('MostBoundID',numpy.int64,1),
-('SnapNum',numpy.int32,1),
-('FileNr',numpy.int32,1),
-('SubhaloIndex',numpy.int32,1),
-('SubHalfMass',numpy.int32,1)
+    ('Descendant',numpy.int32,1),
+    ('FirstProgenitor',numpy.int32,1),
+    ('NextProgenitor',numpy.int32,1),
+    ('FirstHaloInFOFgroup',numpy.int32,1),
+    ('NextHaloInFOFgroup',numpy.int32,1),
+    ('Len',numpy.int32,1),
+    ('M_Mean200',numpy.float32,1),
+    ('M_Crit200',numpy.float32,1),
+    ('M_TopHat',numpy.float32,1),
+    ('Pos',numpy.float32,3),
+    ('Vel',numpy.float32,3),
+    ('VelDisp',numpy.float32,1),
+    ('Vmax',numpy.float32,1),
+    ('Spin',numpy.float32,3),
+    ('MostBoundID',numpy.int64,1),
+    ('SnapNum',numpy.int32,1),
+    ('FileNr',numpy.int32,1),
+    ('SubhaloIndex',numpy.int32,1),
+    ('SubHalfMass',numpy.int32,1)
+])
+
+struct_lgaldbidsinput = numpy.dtype([
+    ('HaloID',numpy.int64,1),
+    ('FileTreeNr',numpy.int64,1),
+    ('FirstProgenitor',numpy.int64,1),
+    ('LastProgenitor',numpy.int64,1),
+    ('NextProgenitor',numpy.int64,1),
+    ('Descendant',numpy.int64,1),
+    ('FirstHaloInFOFgroup',numpy.int64,1),
+    ('NextHaloInFOFgroup',numpy.int64,1),
+    ('Redshift',numpy.float64,1),
+    ('PeanoKey',numpy.int32,1),
+    ('dummy',numpy.int32,1)
+])
+
+struct_lgaldbidsinput_MRII = numpy.dtype([
+    ('HaloID',numpy.int64,1),
+    ('FileTreeNr',numpy.int64,1),
+    ('FirstProgenitor',numpy.int64,1),
+    ('LastProgenitor',numpy.int64,1),
+    ('NextProgenitor',numpy.int64,1),
+    ('Descendant',numpy.int64,1),
+    ('FirstHaloInFOFgroup',numpy.int64,1),
+    ('NextHaloInFOFgroup',numpy.int64,1),
+    ('MainLeafID',numpy.int64,1),
+    ('Redshift',numpy.float64,1),
+    ('PeanoKey',numpy.int32,1),
+    ('dummy',numpy.int32,1)
 ])
 
 tree_properties_used = {}
@@ -59,6 +88,34 @@ def read_lgal_input_tree(folder,file_prefix,firstfile,lastfile,filter_arr,verbos
         output_Halos = numpy.append(output_Halos,addedHalos)
         f.close()
     return (nTrees,nHalos,nTreeHalos,output_Halos)
+
+def read_lgal_input_fulltrees_withids(folder,lastsnap,firstfile,lastfile,verbose):
+    dt = struct_lgalinput
+    nTrees = 0
+    nHalos = 0
+    nTreeHalos = numpy.array([],dtype=numpy.int32)
+    output_Halos = numpy.array([],dtype=struct_lgalinput)
+    output_HaloIDs = numpy.array([],dtype=struct_lgaldbidsinput)
+    for ifile in range(firstfile,lastfile+1):
+        filename = folder+'/trees_'+"%d"%(lastsnap)+'.'+"%d"%(ifile)
+        f = open(filename,"rb")
+        this_nTrees = numpy.fromfile(f,numpy.int32,1)[0]
+        nTrees += this_nTrees
+        this_nHalos = numpy.fromfile(f,numpy.int32,1)[0]
+        nHalos += this_nHalos
+        if(verbose):
+            print "File ", ifile," nHalos = ",this_nHalos
+        addednTreeHalos = numpy.fromfile(f,numpy.int32,this_nTrees)
+        nTreeHalos = numpy.append(nTreeHalos,addednTreeHalos)
+        this_addedHalos = numpy.fromfile(f,struct_lgalinput,this_nHalos)
+        output_Halos = numpy.append(output_Halos,this_addedHalos)
+        f.close()
+        filename = folder+'/tree_dbids_'+"%03d"%(lastsnap)+'.'+"%d"%(ifile)
+        f = open(filename,"rb")
+        this_addedHalos = numpy.fromfile(f,struct_lgaldbidsinput,this_nHalos)
+        output_Halos = numpy.append(output_HaloIDs,this_addedHalos)
+        f.close()
+    return (nTrees,nHalos,nTreeHalos,output_Halos,output_HaloIDs)
 
 def read_lgaltree_advance(folder,file_prefix,firstfile,lastfile,filter_arr,dt,verbose):
     nHalos = 0
