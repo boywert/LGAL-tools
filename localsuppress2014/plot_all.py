2b17e6606121a50ca06ab559b520c5195f1e212a
Chaichalit Srisawat <boywert@gmail.com>
2015-07-29 19:33:47 +0100

    quick commit

--- localsuppress2014/plot_all.py
+++ localsuppress2014/plot_all.py
@@ -38,10 +38,10 @@ def loadfilter(structfile):
 
 h0 = 0.7
 gadgetmass = 1.e10
-model_names = ["noreionization","okamoto","patchyG","patchy8","patchy9"]
-struct_file = ["/mnt/lustre/scratch/cs390/47Mpc/outputs/no_reionization/inputs/LGalaxyStruct.py","/mnt/lustre/scratch/cs390/47Mpc/outputs/no_reionization/inputs/LGalaxyStruct.py","/mnt/lustre/scratch/cs390/47Mpc/couple/model_5/sams/480.00/LGalaxyStruct.py","/mnt/lustre/scratch/cs390/47Mpc/couple/model_7/sams/480.00/LGalaxyStruct.py","/mnt/lustre/scratch/cs390/47Mpc/couple/model_8/sams/480.00/LGalaxyStruct.py"]
+model_names = ["nosup","nosup_nothres","patchyG","patchyG_nothres"]
+struct_file = ["/mnt/lustre/scratch/cs390/47Mpc/outputs/no_reionization/inputs/LGalaxyStruct.py","/mnt/lustre/scratch/cs390/47Mpc/outputs/no_reionization_nothres/inputs/LGalaxyStruct.py","/mnt/lustre/scratch/cs390/47Mpc/couple/model_5/sams/480.00/LGalaxyStruct.py","/mnt/lustre/scratch/cs390/47Mpc/couple/model_5_nothres/sams/480.00/LGalaxyStruct.py"]
 
-use_model = [True,True,True,True,True]
+use_model = [True,True,True,True]
 dt = []
 filter = []
 for i in range(len(struct_file)):
@@ -49,9 +49,9 @@ for i in range(len(struct_file)):
     filter.append(f)
     dt.append(t)
 
-model_labels = ["No Suppression","Okamoto et al.(2008)","Patchy Suppression (Gradual)","Patchy Suppression (cuttoff at 8)","Patchy Suppression (cuttoff at 9)"]
-model_paths = ["/mnt/lustre/scratch/cs390/47Mpc/outputs/no_reionization/","/mnt/lustre/scratch/cs390/47Mpc/outputs/okamoto/","/mnt/lustre/scratch/cs390/47Mpc/couple/model_5/sams/480.00/","/mnt/lustre/scratch/cs390/47Mpc/couple/model_7/sams/480.00/","/mnt/lustre/scratch/cs390/47Mpc/couple/model_8/sams/480.00/"]
-model_plot_patterns = ['r--','g--','b--','y--','k--']
+model_labels = ["No Suppression","No Suppression nothreshold","Patchy Suppression (Gradual)","Patchy Suppression (Gradual,nothres)"]
+model_paths = ["/mnt/lustre/scratch/cs390/47Mpc/outputs/no_reionization/","/mnt/lustre/scratch/cs390/47Mpc/outputs/no_reionization_nothres/","/mnt/lustre/scratch/cs390/47Mpc/couple/model_5/sams/480.00/","/mnt/lustre/scratch/cs390/47Mpc/couple/model_5_nothres/sams/480.00/"]
+model_plot_patterns = ['r--','g--','b--','y--']
 
 
 #filter model
