80fbd4af1440f927d1eace2ff623ef383596f9c0
Chaichalit Srisawat <boywert@gmail.com>
2015-08-24 19:30:18 +0100

    quick commit

--- localsuppress2014/gen_mcmc.py
+++ localsuppress2014/gen_mcmc.py
@@ -27,9 +27,8 @@ def main():
         for j in range(nbins):
             min = min_m+j*delta_logm
             max = min+delta_logm
-            print numpy.log10(output_Halos[rootindex]['M_Crit200']*gadget_m_conv)
-            #choose_list = numpy.where((numpy.log10(output_Halos[rootindex]['M_Crit200']*gadget_m_conv/hubble_h) >=min) & (numpy.log10(output_Halos[rootindex]['M_Crit200']*gadget_m_conv/hubble_h) <=max))
-            #print choose_list
+            choose_list = numpy.where((numpy.log10(output_Halos[rootindex]['M_Crit200']*gadget_m_conv/hubble_h) >=min))
+            print choose_list
 
     return 0
 
