d44f51c6f442c97c7d911d2c03b411cc96f60ecd
Chaichalit Srisawat <boywert@gmail.com>
2015-08-24 22:12:08 +0100

    quick commit

--- localsuppress2014/gen_mcmc.py
+++ localsuppress2014/gen_mcmc.py
@@ -14,7 +14,7 @@ min_m = 8.
 max_m = 10.
 nbins = 20
 delta_logm = (max_m-min_m)/nbins
-sample_bin = 20
+sample_bin = 2
 def main():
     folder = "/mnt/lustre/scratch/cs390/47Mpc/treedata/"
     file_prefix = "trees_%03d." % (lastsnap)
@@ -25,6 +25,7 @@ def main():
     tot_ntreehalos = numpy.array([],dtype=numpy.int32)
     tot_output_halos = numpy.array([],dtype=struct_lgalinput)
     tot_output_haloids_mcmc  = numpy.array([],dtype=numpy.int64)
+    tot_weight = numpy.array([],dtype=numpy.float64)
     
     (nTrees,nHalos,nTreeHalos,output_Halos,output_HaloIDs) = read_lgal_input_fulltrees_withids(folder,lastsnap,firstfile,lastfile,verbose=True)
     rootindex = numpy.cumsum(nTreeHalos)-nTreeHalos
@@ -32,20 +33,21 @@ def main():
         lbound = min_m+j*delta_logm
         rbound = lbound+delta_logm
         print lbound,rbound
-        choose_list = numpy.where((numpy.log10(output_Halos[rootindex]['M_Crit200']*gadget_m_conv/hubble_h) <=rbound) & (numpy.log10(output_Halos[rootindex]['M_Crit200']*gadget_m_conv/hubble_h) >=lbound))[0]
-        choose_list = random.sample(choose_list,min(len(choose_list),sample_bin))
+        r_list = numpy.where((numpy.log10(output_Halos[rootindex]['M_Crit200']*gadget_m_conv/hubble_h) <=rbound) & (numpy.log10(output_Halos[rootindex]['M_Crit200']*gadget_m_conv/hubble_h) >=lbound))[0]
+        choose_list = random.sample(r,min(len(r_list),sample_bin))
         for h in choose_list:
             tot_ntrees += 1
             tot_nhalos += nTreeHalos[h]
             tot_ntreehalos = numpy.append(tot_ntreehalos,nTreeHalos[h])
             tot_output_halos = numpy.append(tot_output_halos,output_Halos[rootindex[h]:rootindex[h+1]])
             tot_output_haloids_mcmc = numpy.append(tot_output_haloids_mcmc,output_HaloIDs[rootindex[h]]["HaloID"])
-
+            tot_weight = numpy.append(tot_weight,float(sample_bin)/len(r_list))
     print tot_ntrees
     print tot_nhalos
     print tot_ntreehalos
     print tot_output_halos
     print tot_output_haloids_mcmc
+    print tot_weight
     return 0
 
 if __name__=="__main__":
