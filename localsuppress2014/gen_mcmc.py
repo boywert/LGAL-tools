145d3eef8d6ee1ce6c40bb52f02b1512e8682a02
Chaichalit Srisawat <boywert@gmail.com>
2015-08-24 16:39:54 +0100

    quick commit

--- localsuppress2014/gen_mcmc.py
+++ localsuppress2014/gen_mcmc.py
@@ -23,10 +23,10 @@ def main():
         lastfile = i
         (nTrees,nHalos,nTreeHalos,output_Halos,output_HaloIDs) = read_lgal_input_fulltrees_withids(folder,lastsnap,firstfile,lastfile,verbose=False)
         firsthalo = numpy.cumsum(nTreeHalos)-nTreeHalos
-        print nTrees,nHalos,nTreeHalos
         haloindex = numpy.where(output_Halos['SnapNum'] == lastsnap)
         halos = output_Halos[haloindex]
         haloids = output_Halos[haloindex]
+        print firsthalo
     return 0
 
 if __name__=="__main__":
