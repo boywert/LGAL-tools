79a9a5e9801531a0d17cb541ce3d644563b95535
Chaichalit Srisawat <boywert@gmail.com>
2015-08-24 22:49:37 +0100

    quick commit

--- localsuppress2014/gen_mcmc.py
+++ localsuppress2014/gen_mcmc.py
@@ -35,7 +35,7 @@ def main():
         lastfile = ifile
         (nTrees,nHalos,nTreeHalos,output_Halos,output_HaloIDs) = read_lgal_input_fulltrees_withids(folder,lastsnap,firstfile,lastfile,verbose=True)
         rootindex = numpy.cumsum(nTreeHalos)-nTreeHalos
-    
+        print "make total table"
         # compute weight table
         for i in range(lastsnap+1):
             for j in range(nbins):
@@ -43,7 +43,7 @@ def main():
                 rbound = lbound+delta_logm
                 t_list = numpy.where((numpy.log10(output_Halos['M_Crit200']*gadget_m_conv/hubble_h) <=rbound) & (numpy.log10(output_Halos['M_Crit200']*gadget_m_conv/hubble_h) >=lbound) & (output_Halos['SnapNum'] == i) & (output_HaloIDs["HaloID"] == output_HaloIDs["FirstHaloInFOFgroup"]))[0]
                 tot_nbins[j,i] += len(t_list)
-   
+        # sample data
         for j in range(nbins):
             lbound = min_m+j*delta_logm
             rbound = lbound+delta_logm
