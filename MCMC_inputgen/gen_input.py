93d61536fa891542555e0afd133e93bde9905cfc
Chaichalit Srisawat <boywert@gmail.com>
2015-08-28 07:42:10 +0100

    quick commit

--- MCMC_inputgen/gen_input.py
+++ MCMC_inputgen/gen_input.py
@@ -18,7 +18,8 @@ def make_unique(a):
     ind = numpy.lexsort(a.T)
     print ind
     a[numpy.concatenate(([True],numpy.any(a[ind[1:]]!= a[ind[:-1]],axis=1)))]
-    return a
+    print a
+    #return a
 
 def get_mcmc_variables(mcmc_template, output_folder, n_trials):
     fp = open(mcmc_template)
