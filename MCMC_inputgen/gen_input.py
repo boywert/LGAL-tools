c5d1ddbcdcbfc6f352fdc7a14a524cb1b7fd37d5
Chaichalit Srisawat <boywert@gmail.com>
2015-08-28 05:42:09 +0100

    quick commit

--- MCMC_inputgen/gen_input.py
+++ MCMC_inputgen/gen_input.py
@@ -1,5 +1,5 @@
 import sys
-
+import subprocess
 
 def get_template(filename):
     fp = open(filename, "r")
@@ -20,19 +20,29 @@ def get_mcmc_variables(mcmc_template, output_folder):
         if len(data) > 0:
             if (data[0][0] == "%") | (data[0][0] == "#"):
                 mcmc_content.remove(line)
+
     nVars = int(mcmc_content[0].strip())
     mcmc_allvars = {}
+    var_order = []
     for i in range(nVars):
         line = mcmc_content[1+i]
         data = line.strip().split()
         if len(data) > 0:
+            var_order.append(data[0])
             if data[5] == '1':
                 mcmc_allvars[data[0]] = True
             else:
                 mcmc_allvars[data[0]] = False
+    exe = "sort -k 2 -n "+output_folder+"/senna_gt_0.txt | head -n 100"
+    p = subprocess.Popen(exe.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
+    print p
+    # for key in var_order:
+    #     if mcmc_allvars[key]:
+            
     return mcmc_allvars
+
 def main(argv):
-    if len(argv) < 1:
+    if len(argv) < 4:
         print "Usage python "+argv[0]+" <valid_lgal_input_file> <MCMCParameterPriorsAndSwitches.txt> <MCMC_output_folder>"
         exit()
     
