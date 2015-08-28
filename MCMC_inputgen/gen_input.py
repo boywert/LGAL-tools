27c9e43f7c10ecbbb7dffe01b1606d12e920aa94
Chaichalit Srisawat <boywert@gmail.com>
2015-08-28 05:52:10 +0100

    quick commit

--- MCMC_inputgen/gen_input.py
+++ MCMC_inputgen/gen_input.py
@@ -33,10 +33,11 @@ def get_mcmc_variables(mcmc_template, output_folder):
                 mcmc_allvars[data[0]] = True
             else:
                 mcmc_allvars[data[0]] = False
-    exe = "sort -k 2 -n "+output_folder+"/senna_gt_0.txt \| head -n 100"
+    exe = "sort -k 2 -n "+output_folder+"/senna_gt_0.txt"
     print exe.split()
-    p = subprocess.Popen(exe.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0]
-    print p
+    p = subprocess.Popen(exe.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    for i in range(100):
+        print p.stdout.readline()
     # for key in var_order:
     #     if mcmc_allvars[key]:
             
