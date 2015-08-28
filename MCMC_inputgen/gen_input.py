275d98543b166abd3dcc3a85aa20bf34bbde05f6
Chaichalit Srisawat <boywert@gmail.com>
2015-08-28 05:27:19 +0100

    add MCMC_inputgen

---
+++ MCMC_inputgen/gen_input.py
@@ -0,0 +1,44 @@
+import sys
+
+
+def get_template(filename):
+    fp = open(filename, "r")
+    content = fp.readlines()
+    allvars = {}
+    for line in content:
+        data = line.strip().split()
+        if len(data) > 0:
+            if (data[0][0] != "%") & (data[0][0] != "-"):
+                allvars[data[0]] = data[1]
+    return allvars
+def get_mcmc_variables(mcmc_template, output_folder):
+    fp = open(mcmc_template)
+    mcmc_content = fp.readlines()
+
+    for line in mcmc_content:
+        data = line.strip().split()
+        if len(data) > 0:
+            if (data[0][0] == "%") | (data[0][0] == "#"):
+                mcmc_content.remove(line)
+    nVars = int(mcmc_content[0].strip())
+    mcmc_allvars = {}
+    for i in range(nVars):
+        line = mcmc_content[1+i]
+        data = line.strip().split()
+        if len(data) > 0:
+            if data[5] == '1':
+                mcmc_allvars[data[0]] = True
+            else:
+                mcmc_allvars[data[0]] = False
+    return mcmc_allvars
+def main(argv):
+    if len(argv) < 1:
+        print "Usage python "+argv[0]+" <valid_lgal_input_file> <MCMCParameterPriorsAndSwitches.txt> <MCMC_output_folder>"
+        exit()
+    
+    get_mcmc_variables(argv[2], argv[3])
+    template = get_template(argv[1])
+    return 0
+
+if __name__ == "__main__":
+    main(sys.argv)
