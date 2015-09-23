736eb6ed6764821bbddb6247ae91c3f25d8af6e2
Chaichalit Srisawat <boywert@gmail.com>
2015-09-23 02:31:53 +0100

    quick commit

--- localsuppress2014/sfr_m200.py
+++ localsuppress2014/sfr_m200.py
@@ -94,13 +94,15 @@ def plot_uv_z8():
         rangen = (7.5,11.5)
         bins = 40
         gal[index] = gal[index][numpy.where((gal[index]["HaloM_Crit200"]>0.))]
+
         nummax= numpy.nanmax(gal[index]["NPhotReion"])
         gal[index]["NPhotReion"] = numpy.clip(gal[index]["NPhotReion"]+numpy.log10(SEC_PER_YEAR),0.0,nummax)
+        
         total_sfr =  numpy.log10(gal[index]["Sfr"].astype(numpy.float64)*Msun2kg/h_mass)
         nummax2= numpy.nanmax(total_sfr)
         total_sfr = numpy.clip(total_sfr,0.0,nummax2)
-        avg = numpy.sum(gal[index]["NPhotReion"] - numpy.log10(gal[index]["Sfr"]*Msun2kg/h_mass),dtype=numpy.float64)/len(total_sfr)
-        print "avg = ",10.**avg
+        avg = numpy.sum(gal[index]["NPhotReion"] - total_sfr),dtype=numpy.float64)/len(total_sfr)
+        print index,"avg = ",10.**avg
         sum_logphoton[index] = numpy.histogram(numpy.log10(gal[index]["HaloM_Crit200"]*1.e10),range=rangen,bins=bins,weights=gal[index]["NPhotReion"].astype(numpy.float64) - total_sfr)
         ssfr = gal[index]["Sfr"]/(gal[index]["HaloM_Crit200"]*1.e10/hubble_h)
         ssfr = numpy.nan_to_num(ssfr)
