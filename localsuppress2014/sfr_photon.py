aa97b96db593d51c997c30f6889065fdca5747fc
Chaichalit Srisawat <boywert@gmail.com>
2015-09-23 14:10:08 +0100

    quick commit

--- localsuppress2014/sfr_photon.py
+++ localsuppress2014/sfr_photon.py
@@ -103,7 +103,7 @@ def plot_uv_z8():
         total_sfr = numpy.clip(total_sfr,0.0,nummax2)
         avg = numpy.sum(gal[index]["NPhotReion"] - total_sfr,dtype=numpy.float64)/len(total_sfr)
         print index,"avg = ",10.**avg
-        sum_logphoton[index] = numpy.histogram(numpy.log10(gal[index]["HaloM_Crit200"]*1.e10),range=rangen,bins=bins,weights=gal[index]["NPhotReion"].astype(numpy.float64) - numpy.log10(gal[index]["Sfr"].astype(numpy.float64)*Msun2kg/h_mass))
+        sum_logphoton[index] = numpy.histogram(numpy.log10(gal[index]["HaloM_Crit200"]*1.e10),range=rangen,bins=bins,weights=gal[index]["NPhotReion"].astype(numpy.float64) )
         ssfr = gal[index]["Sfr"]/(gal[index]["HaloM_Crit200"]*1.e10/hubble_h)
         ssfr = numpy.nan_to_num(ssfr)
         sum_SFR[index] = numpy.histogram(numpy.log10(gal[index]["HaloM_Crit200"]*1.e10),range=rangen,bins=bins,weights=gal[index]["Sfr"])
