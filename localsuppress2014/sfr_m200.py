efecf6f2c411789ed20494179f970a343640ca3f
Chaichalit Srisawat <boywert@gmail.com>
2015-09-23 02:28:38 +0100

    quick commit

--- localsuppress2014/sfr_m200.py
+++ localsuppress2014/sfr_m200.py
@@ -99,8 +99,8 @@ def plot_uv_z8():
         total_sfr =  numpy.log10(gal[index]["Sfr"].astype(numpy.float64)*Msun2kg/h_mass)
         nummax2= numpy.nanmax(total_sfr)
         total_sfr = numpy.clip(total_sfr,0.0,nummax2)
-        avg = numpy.sum(10**gal[index]["NPhotReion"]/gal[index]["Sfr"],dtype=numpy.float64)/len(total_sfr)
-        print "avg = ",avg
+        avg = numpy.sum(gal[index]["NPhotReion"] - numpy.log10(gal[index]["Sfr"]*Msun2kg/h_mass),dtype=numpy.float64)/len(total_sfr)
+        print "avg = ",10.**avg
         sum_logphoton[index] = numpy.histogram(numpy.log10(gal[index]["HaloM_Crit200"]*1.e10),range=rangen,bins=bins,weights=gal[index]["NPhotReion"].astype(numpy.float64) - total_sfr)
         ssfr = gal[index]["Sfr"]/(gal[index]["HaloM_Crit200"]*1.e10/hubble_h)
         ssfr = numpy.nan_to_num(ssfr)
