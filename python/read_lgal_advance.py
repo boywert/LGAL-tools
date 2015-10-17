3c133f9cefbc4249103cba2bc05af68538ec649b
Chaichalit Srisawat <boywert@gmail.com>
2015-10-17 01:43:22 +0100

    quick commit

--- python/read_lgal_advance.py
+++ python/read_lgal_advance.py
@@ -182,8 +182,10 @@ def read_lgaltree_advance(folder,file_prefix,firstfile,lastfile,filter_arr,dt,ve
 
     return (nHalos,output_Galaxy)
 
+# This function return (nTrees,nHalos,nTreeHalos,Galaxy)
+# The input are (folder,file_prefix,firstfile,lastfile [,filter_arr])
 
-def readsnap_lgal_advance2(folder,file_prefix,firstfile,lastfile,filter_arr,dt,verbose):
+def readsnap_lgal_advance(folder,file_prefix,firstfile,lastfile,filter_arr,dt,verbose=1):
     startx = time.time()
     nTrees = 0
     nHalos = 0
@@ -238,44 +240,4 @@ def readsnap_lgal_advance2(folder,file_prefix,firstfile,lastfile,filter_arr,dt,v
     return (nTrees,nHalos,nTreeHalos,output_Galaxy)
 
 
-
-# This function return (nTrees,nHalos,nTreeHalos,Galaxy)
-# The input are (folder,file_prefix,firstfile,lastfile [,filter_arr])
-def readsnap_lgal_advance(folder,file_prefix,firstfile,lastfile,filter_arr,dt,verbose=1):
-    startx = time.time()
-    nTrees = 0
-    nHalos = 0
-    nTreeHalos = numpy.array([],dtype=numpy.int32)
-    filter_tuple = []
-    for prop in dt.names:
-        if(filter_arr[prop] is True):
-            filter_tuple.append((prop,dt[prop]))
-    filter_dtype = numpy.dtype(filter_tuple)
-    output_Galaxy = numpy.array([],dtype=filter_dtype)
-    for ifile in range(firstfile,lastfile+1):
-        start = time.time()
-        filename = folder+'/'+file_prefix+"_"+"%d"%(ifile)
-        f = open(filename,"rb")
-        this_nTrees = numpy.fromfile(f,numpy.int32,1)[0]
-        nTrees += this_nTrees
-        this_nHalos = numpy.fromfile(f,numpy.int32,1)[0]
-        nHalos += this_nHalos
-        if(verbose == 2):
-            print "File ", ifile," nGals = ",this_nHalos
-        addednTreeHalos = numpy.fromfile(f,numpy.int32,this_nTrees)
-        nTreeHalos = numpy.append(nTreeHalos,addednTreeHalos)
-        this_addedGalaxy = numpy.fromfile(f,dt,this_nHalos)
-        addedGalaxy = numpy.zeros(this_nHalos,dtype=filter_dtype)
-        for prop in dt.names:
-            if(filter_arr[prop] is True):
-                addedGalaxy[prop] = this_addedGalaxy[prop]
-        output_Galaxy = numpy.append(output_Galaxy,addedGalaxy)
-        f.close()
-        end = time.time()
-        if(verbose == 2):
-            print end-start,"s"
-    endx = time.time()
-    if(verbose > 0):
-        print "Read ",folder,"file",firstfile,"-",lastfile,":",endx-startx,"s"        
-    return (nTrees,nHalos,nTreeHalos,output_Galaxy)
 
