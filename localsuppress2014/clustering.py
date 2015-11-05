ce678986d682e3322ddb63bc396709ae90e54c9d
Chaichalit Srisawat <boywert@gmail.com>
2015-11-05 21:41:04 +0000

    quick commit

--- localsuppress2014/clustering.py
+++ localsuppress2014/clustering.py
@@ -84,7 +84,7 @@ def plot_xi(z):
         if not index in gal:
             if rank == 0:
                 (nTrees[index],nGals[index],nTreeGals[index],gal[index]) = read_lgal.readsnap_lgal_advance(model_paths[i],file_prefix,firstfile,lastfile,filter[i],dt[i],1)
-                gal[index] = gal[index][numpy.where((gal[index]["Mag"][:,5]<17.0) & (gal[index]["Type"] == 0))]
+                gal[index] = gal[index][numpy.where((gal[index]["Mag"][:,5]<-17.0) & (gal[index]["Type"] == 0))]
                 #gal[index] = gal[index][numpy.where((gal[index]["Type"] == 0))]
                 data = gal[index]["Pos"]
             else:
