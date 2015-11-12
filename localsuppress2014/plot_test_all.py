1898ea1b92ab8fba6aed2a9b00f0a6ba764fd12a
Chaichalit Srisawat <boywert@gmail.com>
2015-11-12 06:42:27 +0000

    quick commit

--- localsuppress2014/plot_test_all.py
+++ localsuppress2014/plot_test_all.py
@@ -207,12 +207,12 @@ def plot_uv_z7():
     for i in range(len(model_names)):
         index = model_names[i]
         ax.plot(luvlf_x[index],luvlf_y[index],model_plot_patterns[i],label=model_labels[i])
-    leg = ax.legend(loc='best', handlelength = 10,ncol=1, fancybox=True, prop={'size':10})
+    leg = ax.legend(loc='best', handlelength = 7,ncol=1, fancybox=True, prop={'size':10})
     leg.get_frame().set_linewidth(0)
     ax.set_xlabel(r"M1600 - 5log(h)")
-    ax.set_ylabel(r"$\mathrm{\Phi (Mpc^{-3} Mag^-1)}$")
+    ax.set_ylabel(r"$\mathrm{\Phi[Mpc^{-3} Mag^-1]}$")
     ax.set_yscale("log")
-    ax.set_xlim([-21,-15])
+    ax.set_xlim([-21,-16])
     fig.savefig("uv_l_z7.pdf",bbox_inches='tight',pad_inches=0)
     plt.close(fig)
 
