a97896424b12546f17d1f5cf6b2b96f9e56bd525
Chaichalit Srisawat <boywert@gmail.com>
2016-01-03 01:06:42 +0000

    quick commit

--- localsuppress2014/sfr_m200.py
+++ localsuppress2014/sfr_m200.py
@@ -77,8 +77,9 @@ model_paths = model_paths_tmp
 
 pylab.rc('text', usetex=True)
 pylab.rc('lines', linewidth=2)
-pylab.rc('ytick',major.size=8)
-pylab.rc('xtick',major.size=8)
+plt.rcParams('ytick.major.size') = 8
+plt.rcParams('xtick.major.size') = 8
+
 zlist = open(zlistfile,"r").readlines()
 
 
