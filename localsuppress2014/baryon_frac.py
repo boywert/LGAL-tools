c9bc1d6119973bda1ace98acca23a07815797e8b
Chaichalit Srisawat <boywert@gmail.com>
2016-01-04 13:24:00 +0000

    quick commit

--- localsuppress2014/baryon_frac.py
+++ localsuppress2014/baryon_frac.py
@@ -115,9 +115,9 @@ def plot_z(z,models,ax,plt_leg):
         index = models.model_names[i]
         ax.plot(m200c[index],sum_baryons[index][0]/N[index][0],color=models.model_plot_colors[i],linestyle=models.model_plot_patterns[i],label=models.model_labels[i])
         mean = sum_baryons[index][0]/N[index][0]
-        sd =  sum_baryons_sq[index][0]/N[index][0] - mean**2
+        sd =  numpy.sqrt(numpy.fabs(sum_baryons_sq[index][0]/N[index][0] - mean**2))
         print mean,sd
-        ax.fill_between(m200c[index], sum_baryons[index][0]/N[index][0] - sd, sum_baryons[index][0]/N[index][0] + sd, alpha=0.5, edgecolor='#CC4F1B', facecolor=models.model_plot_colors[i],linewidth=0)
+        ax.fill_between(m200c[index], sum_baryons[index][0]/N[index][0] - sd, sum_baryons[index][0]/N[index][0] + sd, alpha=0.25, edgecolor='#CC4F1B', facecolor=models.model_plot_colors[i],linewidth=0)
     if plt_leg == 1:
         leg = ax.legend(loc='best', handlelength = 10,ncol=1, fancybox=True, prop={'size':10})
         leg.get_frame().set_linewidth(0)
