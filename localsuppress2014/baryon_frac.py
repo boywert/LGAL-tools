909f214a17bfc9e01467b50b4f89b1ac75f4aa18
Chaichalit Srisawat <boywert@gmail.com>
2016-01-05 00:08:14 +0000

    quick commit

--- localsuppress2014/baryon_frac.py
+++ localsuppress2014/baryon_frac.py
@@ -130,11 +130,11 @@ def plot_z(z,models,ax,pos):
     if pos == "l":
         ax.set_ylabel(r"$f_{\mathrm{Baryon}}/f_b$")
     if pos == "l":
-        ax.text(0.5, 0.85, 'stripping 0',
-                verticalalignment='bottom', horizontalalignment='left',
+        ax.text(0.5, 0.9, 'stripping 0',
+                verticalalignment='bottom', horizontalalignment='center',
                 transform=ax.transAxes, fontsize=14)
     else:
-        ax.text(0.5, 0.85, 'stripping 1',
+        ax.text(0.5, 0.9, 'stripping 1',
                 verticalalignment='bottom', horizontalalignment='left',
                 transform=ax.transAxes, fontsize=14)
 
