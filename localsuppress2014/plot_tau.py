0e8c24909833b5017d5fdcea1cc48e4b4f8c9958
Chaichalit Srisawat <boywert@gmail.com>
2016-01-05 05:03:10 +0000

    quick commit

--- localsuppress2014/plot_tau.py
+++ localsuppress2014/plot_tau.py
@@ -31,6 +31,8 @@ def plot_tau():
     for i in range(len(model_names)):
         index = model_names[i]
         tau = numpy.loadtxt(tau_folder+"/"+model_names[i]+".tau")
+        if tau[0,0] == "1":
+            tau[:,0] = tau[:,0] + 1.
         ax.plot(tau[:,0],tau[:,1],color=model_plot_colors[i],linestyle=model_plot_patterns[i],label=model_labels[i])
 
     leg = ax.legend(loc=4, handlelength = 10,ncol=1, fancybox=True, prop={'size':12})
