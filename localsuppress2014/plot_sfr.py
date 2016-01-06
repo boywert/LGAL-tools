7d328c9857c6c0ee200cc5b9766f93578968ca0e
Chaichalit Srisawat <boywert@gmail.com>
2016-01-06 14:19:24 +0000

    quick commit

--- localsuppress2014/plot_sfr.py
+++ localsuppress2014/plot_sfr.py
@@ -196,7 +196,7 @@ def main():
     plot_smf_z6(ax1)
     #plot_smf_z7(ax2)
     plot_smf_z8(ax2)
-    fig.savefig("SMF68.pdf",bbox_inches='tight',pad_inches=0)
+    fig.savefig("SFR68.pdf",bbox_inches='tight',pad_inches=0)
     plt.close(fig)
 
 if __name__=="__main__":
