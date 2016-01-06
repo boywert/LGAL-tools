2b4ecebf0904599a3956b3b7474a65899e2b2018
Chaichalit Srisawat <boywert@gmail.com>
2016-01-06 05:26:57 +0000

    quick commit

--- localsuppress2014/plot_xfrachist.py
+++ localsuppress2014/plot_xfrachist.py
@@ -49,7 +49,7 @@ def plot_xfrac():
     #ax.set_xscale("log")
     ax1.set_ylim([0.0,1.0])
     ax0.set_ylim([1.0,3.0])
-    fig.savefig("xfrachist.pdf",bbox_inches='tight',pad_inches=0)
+    fig.savefig("xfrachist.pdf",bbox_inches='tight')#,pad_inches=0)
     plt.close(fig)
     
 def main():
