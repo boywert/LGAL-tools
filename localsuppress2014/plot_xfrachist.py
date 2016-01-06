3a614166666172fad6d77c0052b7687cb6c60664
Chaichalit Srisawat <boywert@gmail.com>
2016-01-06 05:13:55 +0000

    quick commit

--- localsuppress2014/plot_xfrachist.py
+++ localsuppress2014/plot_xfrachist.py
@@ -26,20 +26,22 @@ plt.rcParams['ytick.major.size'] = 8
 plt.rcParams['xtick.major.size'] = 8
 
 def plot_xfrac():
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
+    fig = plt.figure(figsize=(8, 8)) 
+    gs = gridspec.GridSpec(2, 1, height_ratios=[1, 3])
+    ax0 = plt.subplot(gs[0])
+    ax1 = plt.subplot(gs[1])
     for i in range(len(model_names)):
         index = model_names[i]
         xfrac = numpy.loadtxt(tau_folder+"/"+model_names[i]+"_"+str(model_fesc[i])+".log")
-        ax.plot(xfrac[:,0],xfrac[:,1],color=model_plot_colors[i],linestyle=model_plot_patterns[i],label=model_labels[i])
+        ax1.plot(xfrac[:,0],xfrac[:,1],color=model_plot_colors[i],linestyle=model_plot_patterns[i],label=model_labels[i])
 
-    leg = ax.legend(loc="lower left", handlelength = 7,ncol=1, fancybox=True, prop={'size':12})
+    leg = ax1.legend(loc="lower left", handlelength = 7,ncol=1, fancybox=True, prop={'size':12})
     leg.get_frame().set_linewidth(0)
-    ax.set_xlabel(r"redshift")
-    ax.set_ylabel(r"$\langle x^{\mathrm{v}}_{\mathrm{HII}}\rangle$")
-    ax.set_xlim([6,20])
+    ax1.set_xlabel(r"redshift")
+    ax1.set_ylabel(r"$\langle x^{\mathrm{v}}_{\mathrm{HII}}\rangle$")
+    ax1.set_xlim([6,15])
     #ax.set_xscale("log")
-    ax.set_ylim([0.0,1.0])
+    ax1.set_ylim([0.0,1.0])
     fig.savefig("xfrachist.pdf",bbox_inches='tight',pad_inches=0)
     plt.close(fig)
     
