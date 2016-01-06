6a35a8a597f1c2e736092355523075711b2d7eb9
Chaichalit Srisawat <boywert@gmail.com>
2016-01-06 05:03:37 +0000

    quick commit

--- localsuppress2014/plot_xfrachist.py
+++ localsuppress2014/plot_xfrachist.py
@@ -28,23 +28,16 @@ plt.rcParams['xtick.major.size'] = 8
 def plot_xfrac():
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    x = numpy.arange(0.,20.,0.1)
-    y = tau_e*numpy.ones(len(x))
-    yu = y+delta_tau_e
-    yl = y-delta_tau_e
-    ax.plot(x,y,"k-",linewidth=1)
-    ax.fill_between(x, yu, yl, alpha=0.25, facecolor="#606060",linewidth=0)
-    ax.text(7.5, 0.069, 'PLANCK 2015', fontsize=12)
     for i in range(len(model_names)):
         index = model_names[i]
         xfrac = numpy.loadtxt(tau_folder+"/"+model_names[i]+"_"+str(model_fesc[i])+".log")
         ax.plot(xfrac[:,0],xfrac[:,1],color=model_plot_colors[i],linestyle=model_plot_patterns[i],label=model_labels[i])
 
-    leg = ax.legend(loc=4, handlelength = 10,ncol=1, fancybox=True, prop={'size':12})
+    leg = ax.legend(loc="lower left", handlelength = 7,ncol=1, fancybox=True, prop={'size':12})
     leg.get_frame().set_linewidth(0)
     ax.set_xlabel(r"redshift")
-    ax.set_ylabel(r"$\langle x^v_{\mathrm{HII}}\rangle$")
-    ax.set_xlim([6,15])
+    ax.set_ylabel(r"$\langle x^{\mathrm{v}}_{\mathrm{HII}}\rangle$")
+    ax.set_xlim([6,10])
     ax.set_ylim([0.0,1.0])
     fig.savefig("xfrachist.pdf",bbox_inches='tight',pad_inches=0)
     plt.close(fig)
