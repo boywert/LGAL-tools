441deab60b742b6fa09596f6350f68b75b4694b4
Chaichalit Srisawat <boywert@gmail.com>
2016-01-07 03:55:01 +0000

    quick commit

--- localsuppress2014/clustering.py
+++ localsuppress2014/clustering.py
@@ -130,7 +130,7 @@ def plot_xi(z):
                 verticalalignment='bottom', horizontalalignment='left',
                 transform=ax.transAxes, fontsize=14)
             print "saving fig",slot+"_"+str(abs(mag))+"_xi"+str(z)+".pdf"
-            fig.savefig(slot+"_"+str(abs(mag))+"_xi"+str(z)+".pdf",bbox_inches='tight',pad_inches=0)
+            fig.savefig(slot+"_"+str(abs(mag))+"_xi"+str(z)+".pdf",bbox_inches='tight',pad_inches=0.01)
             print "done"
 def main():
     zi = sys.argv[1]
