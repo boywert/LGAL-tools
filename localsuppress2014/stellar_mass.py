6045b00d883fed664fde46264b122116dcc4ba38
Chaichalit Srisawat <boywert@gmail.com>
2016-01-07 04:39:16 +0000

    quick commit

--- localsuppress2014/stellar_mass.py
+++ localsuppress2014/stellar_mass.py
@@ -99,7 +99,7 @@ def plot_z(z,models,ax,pos):
         index = models.model_names[i]
         if not index in gal:
             (nTrees[index],nGals[index],nTreeGals[index],gal[index]) = read_lgal.readsnap_lgal_advance(models.model_paths[i],file_prefix,firstfile,lastfile,filter[i],dt[i],1)
-        rangen = (8.15,11.5)
+        rangen = (8.0,11.5)
         bins = 40
         gal[index] = gal[index][numpy.where(((gal[index]["BulgeMass"]+gal[index]["DiskMass"]) >0.))]
         mass = gal[index]["Mvir"] #(gal[index]["BulgeMass"]+gal[index]["DiskMass"])
