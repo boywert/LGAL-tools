55e077880b9089ff5339da8c1881323656111c2f
Chaichalit Srisawat <boywert@gmail.com>
2016-01-07 07:35:48 +0000

    quick commit

--- localsuppress2014/size.py
+++ localsuppress2014/size.py
@@ -30,9 +30,12 @@ def plot_size(ax,folder,pos):
     for i in range(len(model_names)):
         index = model_names[i]
         data = numpy.loadtxt(folder+"/"+index)
-        x = []
-        dx = []
-        ax.plot(data[:,0],data[:,3]/numpy.sum(data[:,3]),color=model_plot_colors[i],linestyle=model_plot_patterns[i],label=model_labels[i])
+        x = data[:,0]
+        dx = numpy.zeros(len(x))
+        for i in range(len(x)-1):
+            dx = x[i] - x[i+1]
+        dx[len(x)-1] = x[len(x)-1]
+        ax.plot(x,data[:,3]/numpy.sum(data[:,3])/,color=model_plot_colors[i],linestyle=model_plot_patterns[i],label=model_labels[i])
 
     if pos != "l":
         ax.yaxis.set_ticklabels([])
