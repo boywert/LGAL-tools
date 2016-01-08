006ea125ff63f85e56113c9038388a0f5233398f
Chaichalit Srisawat <boywert@gmail.com>
2016-01-08 14:38:45 +0000

    quick commit

--- localsuppress2014/plot_xi.py
+++ localsuppress2014/plot_xi.py
@@ -13,190 +13,59 @@ import add_observations
 sys.path.append("../python/")
 import read_lgal_advance as read_lgal
 import timeit
+import random
 rank = "0"
-os.system("mkdir -p ../tmp/"+rank)
-def loadfilter(structfile):
-    sys.path.insert(0,"../tmp/"+rank)
-    os.system("cp "+structfile+" ../tmp/"+rank+"/LGalaxyStruct.py")
-    os.system("rm -f ../tmp/"+rank+"/LGalaxyStruct.pyc")
-    reload(LGalaxyStruct)
-    filter = LGalaxyStruct.properties_used
-    for fi in filter:
-        fi = False    
-    filter['Sfr'] = True
-    dt = LGalaxyStruct.struct_dtype
-    return (filter,dt)
-
-dt = []
-filter = []
-for i in range(len(struct_file)):
-    (f,t) = loadfilter(struct_file[i])
-    filter.append(f)
-    dt.append(t)
-
-#filter model
-filter_tmp = []
-dt_tmp = []
-model_names_tmp = []
-struct_file_tmp = []
-model_labels_tmp = []
-model_paths_tmp = []
-for i in range(len(use_model)):
-    if use_model[i]:
-        filter_tmp.append(filter[i])
-        dt_tmp.append(dt[i])
-        model_names_tmp.append(model_names[i])
-        struct_file_tmp.append(struct_file[i])
-        model_labels_tmp.append(model_labels[i])
-        model_paths_tmp.append(model_paths[i])
-
-filter = filter_tmp
-dt = dt_tmp
-model_names = model_names_tmp
-struct_file = struct_file_tmp
-model_labels = model_labels_tmp
-model_paths = model_paths_tmp       
-
-
-
+SEC_PER_YEAR = 3600*24*365.25
+Msun2kg = 1.989e30
+h_mass = 1.6737237e-27 #kg
+ranki = str(random.randint(0,1000000))
+os.system("mkdir -p ../tmp/"+ranki)
 pylab.rc('text', usetex=True)
 pylab.rc('lines', linewidth=2)
 plt.rcParams['ytick.major.size'] = 8
 plt.rcParams['xtick.major.size'] = 8
-zlist = open(zlistfile,"r").readlines()
-
-
-def plot_smf_z8(ax):
-    z = "7.96"
-    file_prefix = "SA_z"+z
-    #firstfile = 0
-    #lastfile = 127
-    config = {}
-
-    try:
-        gal
-    except NameError:
-        gal = {}
-        nTrees = {}
-        nGals = {}
-        nTreeGals = {}
-   
-    sfr_x = {}
-    sfr_y = {}
-   
-    for i in range(len(model_names)):
-        index = model_names[i]
-        if not index in gal:
-            (nTrees[index],nGals[index],nTreeGals[index],gal[index]) = read_lgal.readsnap_lgal_advance(model_paths[i],file_prefix,firstfile,lastfile,filter[i],dt[i],1)
-        (sfr_x[index],sfr_y[index]) = sfr_density_fn(gal[index],mass_min=10**-2,mass_max=10.**2,nbins=20)
+folder = "xi/"
+def plot_size(ax,m,z,pos):    
 
     for i in range(len(model_names)):
         index = model_names[i]
-        ax.plot(sfr_x[index],sfr_y[index],color=model_plot_colors[i],linestyle=model_plot_patterns[i],label=model_labels[i])
+        data = numpy.loadtxt(folder+"/StellarMass_"+index+"_"+m+"_"+z+".txt")
+        ax.plot(data[:,0],data[:,1],color=model_plot_colors[i],linestyle=model_plot_patterns[i],label=model_labels[i])
 
-    ax.set_xlabel(r"$\mathrm{\log_{10} SFR(M_\odot/year)}$")
-    ax.set_xlim([-2,2])
-    ax.set_ylim([1e-5,10])
-    labels = ["",r"$-1.5$",r"$-1.0$",r"$-0.5$",r"$0$",r"$0.5$",r"$1.0$",r"$1.5$",r"$2.0$"]
-    ax.xaxis.set_ticklabels(labels)
-    #ax.set_ylabel(r"$\mathrm{\Phi(Mpc^{-3} dex^{-1}})$")
-    ax.set_yscale("log")     
-    ax.text(0.9, 0.9, 'z = 8',
-            verticalalignment='bottom', horizontalalignment='right',
-            transform=ax.transAxes, fontsize=15)
-    ax.yaxis.set_ticklabels([])
-    
-def plot_smf_z7(ax):
-    z = "6.98"
-    file_prefix = "SA_z"+z
-    #firstfile = 0
-    #lastfile = 127
-    config = {}
 
-    try:
-        gal
-    except NameError:
-        gal = {}
-        nTrees = {}
-        nGals = {}
-        nTreeGals = {}
-   
-    sfr_x = {}
-    sfr_y = {}
-   
-    for i in range(len(model_names)):
-        index = model_names[i]
-        if not index in gal:
-            (nTrees[index],nGals[index],nTreeGals[index],gal[index]) = read_lgal.readsnap_lgal_advance(model_paths[i],file_prefix,firstfile,lastfile,filter[i],dt[i],1)
-        (sfr_x[index],sfr_y[index]) = sfr_density_fn(gal[index],mass_min=10**-2,mass_max=10.**2,nbins=20)
-            
-    add_observations.add_obs_sfr_z6("observations/SFR/",ax)
-    for i in range(len(model_names)):
-        index = model_names[i]
-        ax.plot(sfr_x[index],sfr_y[index],color=model_plot_colors[i],linestyle=model_plot_patterns[i],label=model_labels[i])(smf_x[index],smf_y[index]) 
-    #leg = ax.legend(loc='best', handlelength = 10,ncol=1, fancybox=True, prop={'size':10})
-    #leg.get_frame().set_linewidth(0)
-    #ax.set_xlabel(r"$\mathrm{\log_{10}[m_*/M_\odot]}$")
-    ax.set_ylabel(r"$\mathrm{\Phi(Mpc^{-3} dex^{-1}})$")
-    ax.set_xlim([-0.5,2])
-    ax.set_ylim([1e-5,1e-1])
-    ax.set_yscale("log")     
-    ax.text(0.9, 0.9, 'z = 7',
-            verticalalignment='bottom', horizontalalignment='right',
-            transform=ax.transAxes, fontsize=15)
-    ax.yaxis.set_ticklabels([])
-    
-def plot_smf_z6(ax):
-    z = "6.00"
-    file_prefix = "SA_z"+z
-    #firstfile = 0
-    #lastfile = 127
-    config = {}
-
-    try:
-        gal
-    except NameError:
-        gal = {}
-        nTrees = {}
-        nGals = {}
-        nTreeGals = {}
-
-    sfr_x = {}
-    sfr_y = {}
-
-    for i in range(len(model_names)):
-        index = model_names[i]
-        if not index in gal:
-            (nTrees[index],nGals[index],nTreeGals[index],gal[index]) = read_lgal.readsnap_lgal_advance(model_paths[i],file_prefix,firstfile,lastfile,filter[i],dt[i],1)
-            (sfr_x[index],sfr_y[index]) = sfr_density_fn(gal[index],mass_min=10**-2,mass_max=10.**2,nbins=20)
-            
-    add_observations.add_obs_sfr_z6("observations/SFR/",ax)
-    for i in range(len(model_names)):
-        index = model_names[i]
-        ax.plot(sfr_x[index],sfr_y[index],color=model_plot_colors[i],linestyle=model_plot_patterns[i],label=model_labels[i])
-    leg = ax.legend(loc='lower left', handlelength = 10,ncol=1, fancybox=True, prop={'size':10})
-    leg.get_frame().set_linewidth(0)
-    ax.set_xlabel(r"$\mathrm{\log_{10} SFR(M_\odot/year)}$")
-    ax.set_ylabel(r"$\mathrm{\Phi(Mpc^{-3} dex^{-1}})$")
+    ax.set_xscale("log")
+    #ax.set_xlim([1e-1,100])
+    #ax.set_ylim([1e-2,1])
     ax.set_yscale("log")
-    ax.set_xlim([-,2])
-    ax.set_ylim([1e-5,10])
-    ax.text(0.9, 0.9, 'z = 6',
-            verticalalignment='bottom', horizontalalignment='right',
-            transform=ax.transAxes, fontsize=15)
+    ax.set_xlabel(r"$\xi(\mathrm{r})$")
+    if pos%2 == 1:
+        ax.set_ylabel(r"$\mathrm{r~[h^{-1}Mpc]}$")
+    if pos == "r":
+        leg = ax.legend(loc="lower right", handlelength = 7,ncol=1, fancybox=True, prop={'size':11})
+        leg.get_frame().set_linewidth(0)
 
+    ax.text(0.95, 0.9, z+m,
+            verticalalignment='bottom', horizontalalignment='right',
+            transform=ax.transAxes, fontsize=18)
+    if pos != "l":
+        ax.yaxis.set_ticklabels([])
+        labels = ["",r"$10^{0}$",r"$10^{1}$",r"$10^2$",r"$10^3$"]
+        ax.xaxis.set_ticklabels(labels)
 
     
 def main():
     fig = plt.figure(figsize=(16, 6))
-    ax1 = fig.add_subplot(1,2,1)
-    ax2 = fig.add_subplot(1,2,2)
-    #ax3 = fig.add_subplot(3,1,3)
     plt.subplots_adjust(wspace = 0)
-    plot_smf_z6(ax1)
-    plot_smf_z7(ax2)
-    #plot_smf_z8(ax2)
-    fig.savefig("SFR67_cal.pdf",bbox_inches='tight',pad_inches=0)
+    ax1 = fig.add_subplot(221)
+    ax2 = fig.add_subplot(222)
+    ax3 = fig.add_subplot(223)
+    ax4 = fig.add_subplot(224)
+    fig.canvas.draw()
+    plot_size(ax1, "6.0","6.00", 1)
+    plot_size(ax2, "6.0","9.03", 2)
+    plot_size(ax3, "7.0","6.00", 3)
+    plot_size(ax4, "7.0","9.03", 4)
+    fig.savefig("xi_.pdf",bbox_inches='tight',pad_inches=0.05)
     plt.close(fig)
 
 if __name__=="__main__":
