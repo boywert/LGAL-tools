5e2ac93b24a909f2e8f854586bfe308b249dd56b
Chaichalit Srisawat <boywert@gmail.com>
2016-03-14 13:51:27 +0000

    quick commit

--- caterpilla/smhm.py
+++ caterpilla/smhm.py
@@ -92,9 +92,7 @@ def plot_smf():
     ax = fig.add_subplot(1,1,1)
     for i in range(len(model_names)):
         index = model_names[i]
-        print gal[index]["StellarMass"][numpy.where(gal[index]["StellarMass"] > 0.0)]
-        print len(gal[index]["StellarMass"][numpy.where(gal[index]["StellarMass"] > 0.0)])
-        ax.scatter(numpy.arange(len(gal[index]["Mvir"])),1.e10*(gal[index]["StellarMass"]))
+        ax.scatter(1.e10*(gal[index]["StellarMass"]))
     ax.set_ylabel(r"$\mathrm{\log_{10}[h^{-1}M_*/M_\odot]}$")
     ax.set_xlabel(r"$\mathrm{\log_{10}[h^{-1}M_h/M_\odot]}$")
     ax.set_yscale("log")
