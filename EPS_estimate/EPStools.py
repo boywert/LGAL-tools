a2b3efedccfeb2264e78252733bb96ef360fd28f
Chaichalit Srisawat <boywert@gmail.com>
2016-03-21 13:36:47 +0000

    quick commit

--- EPS_estimate/EPStools.py
+++ EPS_estimate/EPStools.py
@@ -1,9 +1,10 @@
 import matplotlib as plt
+from numpy import *
 plt.use("pgf")
 def figsize(scale):
     fig_width_pt = 469.755                          # Get this from LaTeX using \the\textwidth
     inches_per_pt = 1.0/72.27                       # Convert pt to inch
-    golden_mean = (np.sqrt(5.0)-1.0)/2.0            # Aesthetic ratio (you could change this)
+    golden_mean = (sqrt(5.0)-1.0)/2.0            # Aesthetic ratio (you could change this)
     fig_width = fig_width_pt*inches_per_pt*scale    # width in inches
     fig_height = fig_width*golden_mean              # height in inches
     fig_size = [fig_width,fig_height]
@@ -29,7 +30,6 @@ r"\usepackage[T1]{fontenc}",        # plots will be generated using this preambl
     }
 plt.rcParams.update(pgf_with_latex)
 import commah
-from numpy import *
 from pylab import *
 from folder import *
 from matplotlib.colors import LogNorm
