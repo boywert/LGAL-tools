938fd3be8d0a85594d763d302f745513bdffaf8a
Chaichalit Srisawat <boywert@gmail.com>
2016-03-21 13:34:54 +0000

    quick commit

--- EPS_estimate/EPStools.py
+++ EPS_estimate/EPStools.py
@@ -1,7 +1,24 @@
 import matplotlib as plt
 plt.use("pgf")
-pgf_with_rc_fonts = {"text.usetex": True, "pgf.texsystem": "pdflatex"}
-plt.rcParams.update(pgf_with_rc_fonts)
+pgf_with_latex = {                      # setup matplotlib to use latex for output
+"pgf.texsystem": "pdflatex",        # change this if using xetex or lautex
+"text.usetex": True,                # use LaTeX to write all text
+"font.family": "serif",
+"font.serif": [],                   # blank entries should cause plots to inherit fonts from the document
+"font.sans-serif": [],
+"font.monospace": [],
+"axes.labelsize": 10,               # LaTeX default is 10pt font.
+"text.fontsize": 10,
+"legend.fontsize": 8,               # Make the legend/label fonts a little smaller
+"xtick.labelsize": 8,
+"ytick.labelsize": 8,
+"figure.figsize": figsize(0.9),     # default fig size of 0.9 textwidth
+"pgf.preamble": [
+r"\usepackage[utf8x]{inputenc}",    # use utf8 fonts becasue your computer can handle it :)
+r"\usepackage[T1]{fontenc}",        # plots will be generated using this preamble
+        ]
+    }
+plt.rcParams.update(pgf_with_latex)
 import commah
 from numpy import *
 from pylab import *
