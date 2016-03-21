0bc25885b502f49632ea61e89038b5adbf5442dd
Chaichalit Srisawat <boywert@gmail.com>
2016-03-21 13:36:01 +0000

    quick commit

--- EPS_estimate/EPStools.py
+++ EPS_estimate/EPStools.py
@@ -1,5 +1,14 @@
 import matplotlib as plt
 plt.use("pgf")
+def figsize(scale):
+    fig_width_pt = 469.755                          # Get this from LaTeX using \the\textwidth
+    inches_per_pt = 1.0/72.27                       # Convert pt to inch
+    golden_mean = (np.sqrt(5.0)-1.0)/2.0            # Aesthetic ratio (you could change this)
+    fig_width = fig_width_pt*inches_per_pt*scale    # width in inches
+    fig_height = fig_width*golden_mean              # height in inches
+    fig_size = [fig_width,fig_height]
+    return fig_size
+
 pgf_with_latex = {                      # setup matplotlib to use latex for output
 "pgf.texsystem": "pdflatex",        # change this if using xetex or lautex
 "text.usetex": True,                # use LaTeX to write all text
