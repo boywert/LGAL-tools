22fbbec1a9e4fb90ff87e880d6aa34676eaf9393
Chaichalit Srisawat <boywert@gmail.com>
2016-07-03 16:15:56 +0700

    xfrac > 0.99 for clustering

--- localsuppress2014/clustering_star.py
+++ localsuppress2014/clustering_star.py
@@ -86,8 +86,8 @@ def plot_xi(z):
         if not index in gal:
             if rank == 0:
                 (nTrees[index],nGals[index],nTreeGals[index],gal[index]) = read_lgal.readsnap_lgal_advance(model_paths[i],file_prefix,firstfile,lastfile,filter[i],dt[i],1)
-                if "infall" in index:
-                    gal[index]["MagDust"][:,5] = gal[index]["MagDust"][:,5] - 0.87
+                # if "infall" in index:
+                #     gal[index]["MagDust"][:,5] = gal[index]["MagDust"][:,5] - 0.87
     comm.Barrier()
     slot = "StellarMass"
     m_i = 6.
