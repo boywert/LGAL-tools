f27a87328276695bf231203bc5a5e112d395ca29
Chaichalit Srisawat <boywert@gmail.com>
2016-09-01 16:38:29 +0700

    quick commit

--- localsuppress2014/plot_smf.py
+++ localsuppress2014/plot_smf.py
@@ -96,9 +96,9 @@ def plot_smf_z8(ax):
         ax.plot(smf_x[index],smf_y[index],color=model_plot_colors[i],linestyle=model_plot_patterns[i],label=model_labels[i])
     ax.set_xlabel(r"$\mathrm{\log_{10}[m_*/M_\odot]}$")
     ax.set_ylim([1.e-5,1e1])
-    ax.set_xlim([5,10])
+    ax.set_xlim([7,10])
     #ax.set_ylabel(r"$\mathrm{\Phi(Mpc^{-3} dex^{-1}})$")
-    labels = ["",r"$6$",r"$7$",r"$8$",r"$9$",r"$10$"]
+    labels = ["",r"$8$",r"$9$",r"$10$"]
     ax.xaxis.set_ticklabels(labels)
     ax.set_yscale("log")     
     ax.yaxis.set_ticklabels([])
@@ -180,7 +180,7 @@ def plot_smf_z6(ax):
     ax.set_ylabel(r"$\mathrm{\Phi(Mpc^{-3} dex^{-1}})$")
     ax.set_yscale("log")
     ax.set_ylim([1.e-5,1e1])
-    ax.set_xlim([5,10])
+    ax.set_xlim([7,10])
     ax.text(0.9, 0.9, 'z = 6',
             verticalalignment='bottom', horizontalalignment='right',
             transform=ax.transAxes, fontsize=15)
