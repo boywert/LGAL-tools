534a7b390ff4fb036c9d318e9cbbee6e6da8546e
Chaichalit Srisawat <boywert@gmail.com>
2016-09-03 14:35:26 +0700

    quick commit

--- localsuppress2014/baryon_frac.py
+++ localsuppress2014/baryon_frac.py
@@ -111,9 +111,9 @@ def plot_z(z,models,ax,pos):
                 this_gal = firstgal[ii]+j
                 if ((gal[index][this_gal]['Pos'][0] - gal[index][firstgal[ii]]['Pos'][0])**2.+(gal[index][this_gal]['Pos'][1] - gal[index][firstgal[ii]]['Pos'][1])**2.+(gal[index][this_gal]['Pos'][2] - gal[index][firstgal[ii]]['Pos'][2])**2. < gal[index][firstgal[ii]]['Rvir']**2):
                     cenmass[ii] += total_baryon[this_gal]
-                    print cenmass[ii],total_baryon[this_gal]
+                    #print cenmass[ii],total_baryon[this_gal]
         cenhalomass = gal[index]["Mvir"][firstgal]
-        #print cenmass/cenhalomass
+        print cenmass/cenhalomass
         cond = ~numpy.isnan(cenmass) & ~numpy.isnan(cenhalomass) & (cenhalomass > 0.)
         cenmass = cenmass[cond]
         cenhalomass = cenhalomass[cond]
