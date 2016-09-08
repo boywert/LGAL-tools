73e55159b18541118eb6814e6763d22490fcc5c6
Chaichalit Srisawat <boywert@gmail.com>
2016-09-09 02:26:43 +0700

    quick commit

--- localsuppress2014/mgross_vs_m200c.py
+++ localsuppress2014/mgross_vs_m200c.py
@@ -135,7 +135,7 @@ def plot_z(z,models,ax,pos,label=0,bottom=0,top=0):
         elif pos == "r":
             print models.model_labels[i]
             ax.plot(m200c[index],mean,color=models.model_plot_colors[i],linestyle=models.model_plot_patterns[i],label=models.model_labels[i])
-            ax3.plot(m200c[index],avage,color=models.model_plot_colors[i],linestyle="--",label=models.model_labels[i])
+        ax3.plot(m200c[index],avage,color=models.model_plot_colors[i],linestyle="--",label=models.model_labels[i])
         ax.fill_between(m200c[index], mean - sd, mean + sd, alpha=0.25, edgecolor='#CC4F1B', facecolor=models.model_plot_colors[i],linewidth=0)
     xplot = numpy.arange(0,20.)
     ref = (-9.2+float(z)/30.)+xplot*1.6405
