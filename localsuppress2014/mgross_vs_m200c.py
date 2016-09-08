f3dbef6e997680a2308cfa0a3834c70971660940
Chaichalit Srisawat <boywert@gmail.com>
2016-09-08 15:15:58 +0700

    quick commit

--- localsuppress2014/mgross_vs_m200c.py
+++ localsuppress2014/mgross_vs_m200c.py
@@ -127,20 +127,19 @@ def plot_z(z,models,ax,pos):
             print m200c[index][j],mean[j]
         if pos == "l":
             ax.plot(m200c[index],mean,color=models.model_plot_colors[i],linestyle=models.model_plot_patterns[i])
-        else:
+        elif pos == "r":
             ax.plot(m200c[index],mean,color=models.model_plot_colors[i],linestyle=models.model_plot_patterns[i],label=models.model_labels[i])
         ax.fill_between(m200c[index], mean - sd, mean + sd, alpha=0.25, edgecolor='#CC4F1B', facecolor=models.model_plot_colors[i],linewidth=0)
     ref = (-9.2+float(z)/30.)+1.6405*m200c[index]
     ax.plot(m200c[index],ref,'k--', label = r'$m_{\mathrm{*,gross}} \propto M_{\mathrm{200c}}^{1.64}$')
 
     if pos == "r":
-	post = "r"
         ax.yaxis.set_ticklabels([])
         labels = ["",r"$8.5$",r"$9.0$",r"$9.5$",r"$10.0$",r"$10.5$",r"$11.0$",r"$11.5$",r"$12.0$"]
         ax.xaxis.set_ticklabels(labels)
-    if pos == "r":
-        leg = ax.legend(loc=4, handlelength = 10,ncol=1, fancybox=True, prop={'size':12})
-        leg.get_frame().set_linewidth(0)
+   
+    leg = ax.legend(loc=4, handlelength = 10,ncol=1, fancybox=True, prop={'size':12})
+    leg.get_frame().set_linewidth(0)
     ax.set_ylim([3,10])
     ax.set_xlim([8.0,11.])
     ax.set_xlabel(r"$\log_{10}(M_{\mathrm{200c}}/\mathrm{M_\odot})$")
