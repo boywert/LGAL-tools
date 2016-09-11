b54c19e64c7ed395863637df784a707ea1c85686
Chaichalit Srisawat <boywert@gmail.com>
2016-09-11 13:51:35 +0700

    quick commit

--- localsuppress2014/plot_ejected.py
+++ localsuppress2014/plot_ejected.py
@@ -94,9 +94,9 @@ def plot_hotgas(z,ax,pos):
     for i in range(len(model_names)):
         index = model_names[i]
         ax.plot(smf_x[index],smf_y[index],color=model_plot_colors[i],linestyle=model_plot_patterns[i],label=model_labels[i])
-    ax.set_xlabel(r"$\mathrm{\log_{10}[m_{\rm hot}/M_\odot]}$")
+    ax.set_xlabel(r"$\mathrm{\log_{10}[m_{\rm ejected}/M_\odot]}$")
     ax.set_yscale("log")
-    ax.set_ylim([1.e-2,1e2])
+    ax.set_ylim([1.e-2,1e1])
     ax.set_xlim([6.0,9.0])
     ax.text(0.1, 0.1, 'z = %d'%(int(float(z)+0.5)),
             verticalalignment='bottom', horizontalalignment='left',
@@ -108,7 +108,7 @@ def plot_hotgas(z,ax,pos):
     if pos == 'l':
         ax.set_ylabel(r"$\mathrm{\Phi(Mpc^{-3} dex^{-1}})$")
     if pos == 'r':
-        labels = ["",r"$7.0$",r"$7.5$",r"$8.0$",r"$8.5$",r"$9.0$",r"$9.5$",r"10.0"]
+        labels = ["",r"$6.5$",r"$7.0$",r"$7.5$",r"$8.0$",r"$8.5$",r"$9.0$",r"$9.5$",r"10.0"]
         ax.xaxis.set_ticklabels(labels)
         ax.yaxis.set_ticklabels([])
     ax.set_axisbelow(True)
