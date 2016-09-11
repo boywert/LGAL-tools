f8fe9d047acbdaf4c15122719f2976c75ae97656
Chaichalit Srisawat <boywert@gmail.com>
2016-09-11 22:53:40 +0700

    quick commit

--- localsuppress2014/plot_xfrac_real.py
+++ localsuppress2014/plot_xfrac_real.py
@@ -96,7 +96,7 @@ def plot_reionized(suffix,nrow,ncol,filelist,labellist,doubleflaglist,frac,x,y,z
     #ax.append(pylab.subplot(gs[nrow,1]))
     #cax = fig.add_axes([0.1, 0.1, 0.8,0.05])
     cbar = plt.colorbar(im[0],cax=cax, ticks=[1.1, 2.1],orientation='horizontal')
-    cbar.ax.set_xticklabels(['Low',, 'High'])  # horizontal colorbar
+    cbar.ax.set_xticklabels(['Low', 'High'])  # horizontal colorbar
     fig.savefig(outfile,bbox_inches='tight',pad_inches=0.1)
     plt.close(fig)
 
