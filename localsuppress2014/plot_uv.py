54f741f693bb1cb509f5b2cc05d7679a456f73de
Chaichalit Srisawat <boywert@gmail.com>
2016-09-11 20:20:27 +0700

    quick commit

--- localsuppress2014/plot_uv.py
+++ localsuppress2014/plot_uv.py
@@ -99,7 +99,7 @@ def plot_uv_z8(ax):
     ax.set_ylabel(r"$\mathrm{\Phi[Mpc^{-3} Mag^{-1}]}$")
     ax.set_yscale("log")
     ax.set_xlim([-22,-6])
-    #ax.set_ylim([1e-6,1e-1])
+    ax.set_ylim([1e-6,1])
     ax.text(0.1, 0.9, 'z = 8',
             verticalalignment='bottom', horizontalalignment='left',
             transform=ax.transAxes, fontsize=15)
@@ -148,7 +148,7 @@ def plot_uv_z7(ax):
     ax.set_ylabel(r"$\mathrm{\Phi[Mpc^{-3} Mag^{-1}]}$")
     ax.set_yscale("log")
     ax.set_xlim([-22,-6])
-    #ax.set_ylim([1e-6,1e-1])
+    ax.set_ylim([1e-6,1])
     ax.text(0.1, 0.9, 'z = 7',
             verticalalignment='bottom', horizontalalignment='left',
             transform=ax.transAxes, fontsize=15)
@@ -195,7 +195,7 @@ def plot_uv_z6(ax):
     ax.set_ylabel(r"$\mathrm{\Phi[Mpc^{-3} Mag^{-1}]}$")
     ax.set_yscale("log")
     ax.set_xlim([-22.,-6])
-    #ax.set_ylim([1e-6,1e-1])
+    ax.set_ylim([1e-6,1])
     ax.text(0.1, 0.9, 'z = 6',
             verticalalignment='bottom', horizontalalignment='left',
             transform=ax.transAxes, fontsize=15)
