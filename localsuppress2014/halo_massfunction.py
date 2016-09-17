ba0d2afc977b2ed6d393dbb3948f5436c3f72d66
Chaichalit Srisawat <boywert@gmail.com>
2016-09-17 21:02:19 +0700

    quick commit

--- localsuppress2014/halo_massfunction.py
+++ localsuppress2014/halo_massfunction.py
@@ -12,14 +12,14 @@ hubble_h = 0.7
 
 matplotlib.rc('text', usetex=True)
 matplotlib.rc('savefig', dpi=300)
-zlistfile = "/mnt/lustre/scratch/cs390/47Mpc/snap_z.txt"
-zlist = open(zlistfile,"r").readlines()
-def main():
+def main(folder,file_prefix,zlistfile):
+    # zlistfile = "/mnt/lustre/scratch/cs390/47Mpc/snap_z.txt"
+    zlist = open(zlistfile,"r").readlines()
     filter = read_lgal.tree_properties_used
     filter['M_Crit200'] = True
     filter['SnapNum'] = True
-    folder = "/mnt/lustre/scratch/cs390/47Mpc/treedata/"
-    file_prefix = "trees_075."
+    # folder = "/mnt/lustre/scratch/cs390/47Mpc/treedata/"
+    # file_prefix = "trees_075."
     firstfile = 0
     lastfile = 127
     os.system("mkdir -p pdf")
@@ -29,20 +29,23 @@ def main():
         halos = output_Halos[numpy.where(output_Halos['SnapNum'] == i)]
         if(len(halos) > 0):
             (massfn_x,massfn_y) = mass_fn.M200c_mass_fn(halos,mass_min=1e8,mass_max=1.e13,nbins=50)
-            f = open("pdf/"+zlist[i].strip()+"_massfn.dat","w")
+            f = open(zlist[i].strip()+"_massfn.dat","w")
             for j in range(len(massfn_x)):
                 print >> f,massfn_x[j],massfn_y[j]
             f.close()
-            if(numpy.sum(massfn_y) > 0.00001):
-                fig = plt.figure()
-                ax = fig.add_subplot(111)
-                ax.plot(massfn_x,massfn_y)
-                ax.set_xlabel(r"$\mathrm{M_{200c}[}h^{-1}\mathrm{M_\odot]}$")
-                ax.set_ylabel(r"$\mathrm{\Phi[}h^3\mathrm{Mpc^{-3} dex^-1]}$")
-                ax.set_yscale("log")            
-                fig.savefig("pdf/"+zlist[i].strip()+"_massfn.pdf",bbox_inches='tight',pad_inches=0)
+            # if(numpy.sum(massfn_y) > 0.00001):
+            #     fig = plt.figure()
+            #     ax = fig.add_subplot(111)
+            #     ax.plot(massfn_x,massfn_y)
+            #     ax.set_xlabel(r"$\mathrm{M_{200c}[}h^{-1}\mathrm{M_\odot]}$")
+            #     ax.set_ylabel(r"$\mathrm{\Phi[}h^3\mathrm{Mpc^{-3} dex^-1]}$")
+            #     ax.set_yscale("log")            
+            #     fig.savefig("pdf/"+zlist[i].strip()+"_massfn.pdf",bbox_inches='tight',pad_inches=0)
     return 0
 
 if __name__=="__main__":
-    main()
+    folder = sys.argv[1]
+    prefix = sys.argv[2]
+    zlist = sys.argv[3]
+    main(folder,prefix,zlist)
     
