51456b3183d21618c9a91afea3af238a151408b0
Chaichalit Srisawat <boywert@gmail.com>
2016-12-19 11:23:23 +0700

    quick commit

--- caterpilla/xy_dist.py
+++ caterpilla/xy_dist.py
@@ -91,10 +91,9 @@ def plot_smf():
         if not index in gal:
             (nTrees[index],nGals[index],nTreeGals[index],gal[index]) = read_lgal.readsnap_lgal_advance(model_paths[i],file_prefix,firstfile[i],lastfile[i],filter[i],dt[i],1)
 
-
         fig = plt.figure()
         ax = fig.add_subplot(1,1,1)
-        ax.scatter(gal[index]['Pos'][0],gal[index]['Pos'][0])
+        ax.scatter(gal[index]['Pos'][0],gal[index]['Pos'][1])
         leg = ax.legend(loc='upper left', handlelength = 10,ncol=1, fancybox=True, prop={'size':10})
         leg.get_frame().set_linewidth(0)
         fig.savefig("xy_plane_"+str(i)+".png",bbox_inches='tight',pad_inches=0.1)
