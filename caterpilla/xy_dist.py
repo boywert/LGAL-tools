1332956084c0842a4489a6c1a0584a5688db867b
Chaichalit Srisawat <boywert@gmail.com>
2016-12-19 11:27:26 +0700

    quick commit

--- caterpilla/xy_dist.py
+++ caterpilla/xy_dist.py
@@ -93,8 +93,7 @@ def plot_smf():
 
         fig = plt.figure()
         ax = fig.add_subplot(1,1,1)
-        print gal[index]['Pos'][:,0]
-        ax.scatter(gal[index]['Pos'][0],gal[index]['Pos'][1])
+        ax.scatter(gal[index]['Pos'][:,0],gal[index]['Pos'][:,1])
         # leg = ax.legend(loc='upper left', handlelength = 10,ncol=1, fancybox=True, prop={'size':10})
         # leg.get_frame().set_linewidth(0)
         fig.savefig("xy_plane_"+str(i)+".png",bbox_inches='tight',pad_inches=0.1)
