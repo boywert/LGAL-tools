25a23b6119236cf95a9fb6e0460bd55d248cf759
Chaichalit Srisawat <boywert@gmail.com>
2016-12-20 22:04:51 +0700

    quick commit

--- caterpilla/xy_dist.py
+++ caterpilla/xy_dist.py
@@ -111,10 +111,10 @@ def plot_smf():
         ax = fig.add_subplot(1,1,1)
         gal[index] = gal[index][numpy.where(gal[index]['Pos'][:,2] >51.75275192)]
         gal[index] = gal[index][numpy.where(gal[index]['Pos'][:,2] <51.80275192)]
-        gal[index] = gal[index][numpy.where(gal[index]['Mvir'] > 4)]
-        gal[index] = gal[index][numpy.where(gal[index]['Mvir'] < 13)]
+        gal[index] = gal[index][numpy.where(numpy.log10(gal[index]['Mvir']*1e10) > 4)]
+        gal[index] = gal[index][numpy.where(numpy.log10(gal[index]['Mvir']*1e10) < 13)]
         sc = ax.scatter(gal[index]['Pos'][:,0],gal[index]['Pos'][:,1],c=numpy.log10(gal[index]['Mvir']*1e10),cmap='Reds')
-        cbar = fig.colorbar(sc)
+        cbar = fig.colorbar(sc,boundaries=num)
         ax.set_xlim([47,54])
         ax.set_ylim([43,49])
         ax.set_title(model_labels[i])
