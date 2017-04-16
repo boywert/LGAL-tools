23fe75bd5b44419e17133165a36a9d7b82de817e
Chaichalit Srisawat <boywert@gmail.com>
2017-04-16 14:17:22 +0700

    quick commit

--- localsuppress2014/plot_xfrac_real.py
+++ localsuppress2014/plot_xfrac_real.py
@@ -54,7 +54,7 @@ def get_plot(filename,doubleflag,x,y,z):
 def plot_reionized(suffix,nrow,ncol,filelist,labellist,doubleflaglist,frac,x,y,z):
     outfile = "%3.1f_pic_%d.pdf" % (frac,z[0])
     print "plotting",outfile
-    fig = pylab.figure(figsize=(4*ncol, 4*nrow+0.2))
+    fig = pylab.figure(figsize=(4*ncol, 4*nrow+0.4))
     print "a"
     plt.subplots_adjust(wspace = 0.02)
     print "b"
@@ -65,7 +65,7 @@ def plot_reionized(suffix,nrow,ncol,filelist,labellist,doubleflaglist,frac,x,y,z
     gs_height_ratios = []
     for i in range(nrow):
         gs_height_ratios.append(1.)
-    gs_height_ratios.append(0.03)
+    gs_height_ratios.append(0.3)
     for i in range(ncol):
         gs_width_ratios.append(1.)
     print "f"
