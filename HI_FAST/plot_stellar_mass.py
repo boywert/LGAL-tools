f0fca1a1e7305f0febf1a8dd169e3c17f45a5349
Chaichalit Srisawat <boywert@gmail.com>
2017-06-12 09:40:42 +0700

    quick commit

--- HI_FAST/plot_stellar_mass.py
+++ HI_FAST/plot_stellar_mass.py
@@ -103,7 +103,7 @@ def plot_coldgas(z,ax,sam):
     for i in range(len(model_names)):
         index = model_names[i]
         ax.plot(smf_x[index],smf_y[index],color=model_plot_colors[i],linestyle=model_plot_patterns[i],label=model_labels[i])
-    ax.set_xlabel(r"$\mathrm{\log_{10}[m_{\rm cold}/M_\odot]}$")
+    ax.set_xlabel(r"$\mathrm{\log_{10}[m_{*}/M_\odot]}$")
     ax.set_yscale("log")
     #ax.set_ylim([1.e-5,1e0])
     #ax.set_xlim([6,8])
