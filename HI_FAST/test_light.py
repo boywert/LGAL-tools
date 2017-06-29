a92906f78d4a7c5af1a3a1ec3d7e5bf0bffebab8
Chaichalit Srisawat <boywert@gmail.com>
2017-06-29 09:38:36 +0700

    quick commit

--- HI_FAST/test_light.py
+++ HI_FAST/test_light.py
@@ -99,7 +99,7 @@ def plot_coldgas(z):
             
     for i in range(len(model_names)):
         index = model_names[i]
-        print blas.sdot(gal[index]['Pos'][:,0:3],gal[index]['Pos'][:,0:3],3)
+        print blas.sdot(gal[index]['Pos'],gal[index]['Pos'],3)
 
     
 def main():
