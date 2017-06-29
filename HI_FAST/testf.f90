0972b14395c6280e32a2b9abcaba0d33334c0eac
Chaichalit Srisawat <boywert@gmail.com>
2017-06-29 10:14:55 +0700

    quick commit

---
+++ HI_FAST/testf.f90
@@ -0,0 +1,8 @@
+subroutine blas_3dvsdot(N,A,B)
+  implicit none
+  include 'mkl_blas.fi'
+  include 'mkl_vml.f90'
+  integer :: N
+  real (kind=4) :: A(3,N),B(N)
+  B(:) =  dsqrt(ddot(3, A(1:3,:),1, A(1:3,:), 1))
+end subroutine blas_sdot  
