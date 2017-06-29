a25fe3a0c7f699741c412cc8f0e9cba1ef671ae1
Chaichalit Srisawat <boywert@gmail.com>
2017-06-29 11:30:20 +0700

    quick commit

--- HI_FAST/testf.f90
+++ HI_FAST/testf.f90
@@ -3,7 +3,7 @@ subroutine blas_3dvsdot(N,A,B)
   include 'mkl_blas.fi'
   include 'mkl_vml.f90'
   integer :: N
-  real (kind=4), intent(IN):: A(3,N),B(N)
+  real (kind=4), intent(IN):: A(3,N)
   real (kind=4), intent(OUT):: B(N)
   B(:) =  sqrt(sdot(3, A(1:3,:),1, A(1:3,:), 1))
 end subroutine blas_3dvsdot
