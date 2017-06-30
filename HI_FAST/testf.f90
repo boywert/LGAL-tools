2d6ed5f2367da151fad6eeeb6b4532c1c48ced33
Chaichalit Srisawat <boywert@gmail.com>
2017-06-30 11:24:44 +0700

    quick commit

--- HI_FAST/testf.f90
+++ HI_FAST/testf.f90
@@ -2,7 +2,7 @@ subroutine blas_3dvsdot(N,A,B) bind (c,name='blas_3dvsdot')
   use iso_c_binding
   include 'mkl_blas.fi'
   include 'mkl_vml.f90'
-  integer (c_int), intend(in), value :: N
+  integer (c_int), intent(in), value :: N
   real (c_float), intent(IN):: A(3,N)
   real (c_float), intent(OUT):: B(N)
   B(:) =  sqrt(sdot(3, A(1:3,:),1, A(1:3,:), 1))
