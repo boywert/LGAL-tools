00a4e39a5385373698f998b98e8e71e3da56ee64
Chaichalit Srisawat <boywert@gmail.com>
2017-07-04 08:03:08 +0700

    quick commit

--- HI_FAST/testf.f90
+++ HI_FAST/testf.f90
@@ -3,8 +3,8 @@ subroutine blas_3dvsdot(N,A,B) bind (c,name='blas_3dvsdot')
   include 'mkl_blas.fi'
   include 'mkl_vml.f90'
   integer (c_int), intent(in), value :: N
-  real (c_float), intent(IN):: A(3,N)
+  real (c_float), intent(IN):: A(N,3)
   real (c_float), intent(OUT):: B(N)
-  B(:) =  sqrt(sdot(3, A(1:3,:),1, A(1:3,:), 1))
-  print *, A(:,1)
+  B(:) =  sqrt(sdot(3, A(:,1:3),1, A(:,1:3), 1))
+  print *, A(1,)
 end subroutine blas_3dvsdot
