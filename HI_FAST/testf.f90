d34d62d876db3c3608b1ef020fcb5b124bd0b1eb
Chaichalit Srisawat <boywert@gmail.com>
2017-07-04 08:04:47 +0700

    quick commit

--- HI_FAST/testf.f90
+++ HI_FAST/testf.f90
@@ -6,5 +6,5 @@ subroutine blas_3dvsdot(N,A,B) bind (c,name='blas_3dvsdot')
   real (c_float), intent(IN):: A(N,3)
   real (c_float), intent(OUT):: B(N)
   B(:) =  sqrt(sdot(3, A(:,1:3),1, A(:,1:3), 1))
-  print *, A(1,:)
+  print *, B(1:10)
 end subroutine blas_3dvsdot
