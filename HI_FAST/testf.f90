56f067d79d2c53c92d1a8e7363495dd74ff0119c
Chaichalit Srisawat <boywert@gmail.com>
2017-07-05 06:44:58 +0700

    quick commit

--- HI_FAST/testf.f90
+++ HI_FAST/testf.f90
@@ -34,15 +34,15 @@ subroutine make_sphere(N,boxsize,A,B) bind (c,name='make_sphere')
 end subroutine make_sphere
 
 
-  ! subroutine cart2sphere1(N,A,B) bind (c,name='cart2sphere1')
-  !   use iso_c_binding
-  !   integer (c_int), intent(in), value :: N
-  !   real (c_float), intent(IN):: A(3,N)
-  !   real (c_float), intent(OUT):: B(3,N)
-  !   B(1,:) = sqrt(A(1,1:N)*A(1,1:N)+A(2,1:N)*A(2,1:N)+A(3,1:N)*A(3,1:N))
-  !   B(2,:) = acos(A(3,:)/B(1,:))
-  !   B(3,:) = atan(A(2,:)/A(1,:))
-  ! end subroutine cart2sphere1
+  subroutine cart2sphere1(N,A,B) bind (c,name='cart2sphere1')
+    use iso_c_binding
+    integer (c_int), intent(in), value :: N
+    real (c_float), intent(IN):: A(3,N)
+    real (c_float), intent(OUT):: B(3,N)
+    B(1,:) = sqrt(A(1,1:N)*A(1,1:N)+A(2,1:N)*A(2,1:N)+A(3,1:N)*A(3,1:N))
+    B(2,:) = acos(A(3,:)/B(1,:))
+    B(3,:) = atan(A(2,:)/A(1,:))
+  end subroutine cart2sphere1
 
   ! subroutine cart2sphere2(N,A,B) bind (c,name='cart2sphere2')
   !   use iso_c_binding
