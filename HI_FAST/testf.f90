a37e886a61d0b7d91c12b4b4975c360fa10e6b12
Chaichalit Srisawat <boywert@gmail.com>
2017-07-04 08:30:08 +0700

    quick commit

--- HI_FAST/testf.f90
+++ HI_FAST/testf.f90
@@ -8,5 +8,5 @@ subroutine blas_3dvsdot(N,A,B) bind (c,name='blas_3dvsdot')
   real (c_double), intent(OUT):: B(N)
   call vdabs( N, A(:,1:3), B(:) ) 
   print *, A(1:10,:)
-  print *, B(1:10,:)
+  print *, B(1:10)
 end subroutine blas_3dvsdot
