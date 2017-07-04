84d37b81ccbd692b6383ab620bc2124a5a786514
Chaichalit Srisawat <boywert@gmail.com>
2017-07-04 08:26:27 +0700

    quick commit

--- HI_FAST/testf.f90
+++ HI_FAST/testf.f90
@@ -4,9 +4,9 @@ subroutine blas_3dvsdot(N,A,B) bind (c,name='blas_3dvsdot')
   include 'mkl_vml.f90'
   integer (c_int), intent(in), value :: N
   real (c_double), intent(IN):: A(N,3)
-  real (c_double) :: AA(3,N)
+  real (c_double) :: AA(N,3)
   real (c_double), intent(OUT):: B(N)
-  AA = transpose(A)
-  B(:) =  ddot(3, AA(1:3,:),1, AA(1:3,:), 1)
+  AA = A*A
+  call vdabs( 3, AA(*,1:3), B(*) ) 
   print *, B(1:10)
 end subroutine blas_3dvsdot
