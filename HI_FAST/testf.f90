f4b3e7b3ab8adb4fbc8cda9f5840e4b7f1613f34
Chaichalit Srisawat <boywert@gmail.com>
2017-07-04 11:32:07 +0700

    quick commit

--- HI_FAST/testf.f90
+++ HI_FAST/testf.f90
@@ -7,7 +7,7 @@ subroutine cart2sphere1(N,A,B) bind (c,name='cart2sphere1')
   real (c_double), intent(IN):: A(3,N)
   real (c_double) :: AA(3,N)
   real (c_double), intent(OUT):: B(3,N)
-  do i = 1,100
+  do i = 1,1000
      B(1,:) = dsqrt(A(1,1:N)*A(1,1:N)+A(2,1:N)*A(2,1:N)+A(3,1:N)*A(3,1:N))
      B(2,:) = acos(A(3,:)/B(1,:))
      B(3,:) = atan(A(2,:)/A(1,:))
@@ -22,7 +22,7 @@ subroutine cart2sphere2(N,A,B) bind (c,name='cart2sphere2')
   real (c_double), intent(IN):: A(3,N)
   real (c_double) :: AA(3,N)
   real (c_double), intent(OUT):: B(3,N)
-  do i = 1,100
+  do i = 1,1000
      call vdsqrt(N,A(1,1:N)*A(1,1:N)+A(2,1:N)*A(2,1:N)+A(3,1:N)*A(3,1:N),B(1,:))
      call vdacos(N,A(3,:)/B(1,:),B(2,:))
      call vdatan(N,A(2,:)/A(1,:),B(3,:))
