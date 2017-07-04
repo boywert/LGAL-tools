7a49a854c752a224fda96ba45a23e0aaaf73d1e0
Chaichalit Srisawat <boywert@gmail.com>
2017-07-04 11:30:33 +0700

    quick commit

--- HI_FAST/test_light.py
+++ HI_FAST/test_light.py
@@ -105,12 +105,15 @@ def plot_coldgas(z):
 
         c = numpy.empty((nGals[index],3),dtype=numpy.float64)
         pos =  gal[index]['Pos'].astype(numpy.float64)
-        print "Python",pos
         start = timer()
-        mymodule.blas_3dvsdot(c_int(nGals[index]),pos.ctypes.data_as(POINTER(c_double)),c.ctypes.data_as(POINTER(c_double)))
+        mymodule.cart2sphere1(c_int(nGals[index]),pos.ctypes.data_as(POINTER(c_double)),c.ctypes.data_as(POINTER(c_double)))
         end = timer()
-        print(end - start)/100.0
-    
+        print "fortran:", (end - start)/100.0
+        start = timer()
+        mymodule.cart2sphere2(c_int(nGals[index]),pos.ctypes.data_as(POINTER(c_double)),c.ctypes.data_as(POINTER(c_double)))
+        end = timer()
+        print "mkl vector fortran:", (end - start)/100.0
+        
 def main():
     plot_coldgas(0.0)
 
