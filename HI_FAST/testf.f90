6462335570b087e110616b0e8ffe20cba3e3898e
Chaichalit Srisawat <boywert@gmail.com>
2017-07-05 06:55:48 +0700

    quick commit

--- HI_FAST/testf.f90
+++ HI_FAST/testf.f90
@@ -38,9 +38,23 @@ subroutine make_sphere(N,boxsize,A,B) bind (c,name='make_sphere')
   real (c_float), intent(IN), value :: boxsize
   real (c_float), intent(IN):: A(3,N)
   real (c_float), intent(OUT):: B(3,8*N)
-  B(1,1:N) = sqrt(A(1,1:N)*A(1,1:N)+A(2,1:N)*A(2,1:N)+A(3,1:N)*A(3,1:N))
-  B(2,1:N) = acos(A(3,:)/B(1,:))
-  B(3,1:N) = atan(A(2,:)/A(1,:))
+  real (c_float), allocatable :: AC(:,:)
+  integer :: i,j,k,l
+  allocate(AC(3,8*N))
+  do i=1,2
+     do j=1,2
+        do k=1,2
+           index = (i-1)*2*2 + (j-1)*2 + k - 1
+           do l=1,N
+              AC(1:3,index*N+l) = A(1:3,l) - (/ (i-1), (j-1), (k-1) /)*boxsize
+           end do
+        end do
+     end do
+  end do
+  B(1,1:N) = sqrt(AC(1,1:N)*AC(1,1:N)+AC(2,1:N)*AC(2,1:N)+AC(3,1:N)*AC(3,1:N))
+  B(2,1:N) = acos(AC(3,:)/B(1,:))
+  B(3,1:N) = atan(AC(2,:)/AC(1,:))
+  deallocate(AC)
 end subroutine make_sphere
 
 subroutine cart2sphere1(N,A,B) bind (c,name='cart2sphere1')
