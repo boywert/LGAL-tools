00156408f2c8699f54f722ebcb1d4342cb249a39
Chaichalit Srisawat <boywert@gmail.com>
2017-07-06 17:35:34 +0700

    quick commit

--- HI_FAST/testf.f90
+++ HI_FAST/testf.f90
@@ -48,12 +48,12 @@ subroutine make_sphere(N,boxsize,A,B) bind (c,name='make_sphere')
         do k=1,2
            index = (i-1)*2*2 + (j-1)*2 + k - 1
            if ((i .eq. 1) .and. (j .eq. 1) .and. (k .eq. 1)) then
-              B(:,index*N+1:index*N+N) = cart2sphere1(N,AC)
+              call cart2sphere1(N,AC,B(1:3,index*N+1:index*N+N))
            else
               AC(1,:) = A(1,:) - (i-1)*boxsize
               AC(2,:) = A(2,:) - (j-1)*boxsize
               AC(3,:) = A(3,:) - (k-1)*boxsize
-              B(1:3,index*N+1:index*N+N) = cart2sphere1(N,AC)
+              call cart2sphere1(N,AC,B(1:3,index*N+1:index*N+N))
            end if
         end do
      end do
@@ -62,15 +62,15 @@ subroutine make_sphere(N,boxsize,A,B) bind (c,name='make_sphere')
   deallocate(AC)
 end subroutine make_sphere
 
-function cart2sphere1(N,A) bind (c,name='cart2sphere1')
+subroutine cart2sphere1(N,A,B) bind (c,name='cart2sphere1')
   use iso_c_binding
   integer (c_int), intent(in), value :: N
   real (c_float), intent(IN):: A(3,N)
-  real (c_float), intent(OUT):: cart2sphere1(3,N)
-  cart2sphere1(1,:) = sqrt(A(1,1:N)*A(1,1:N)+A(2,1:N)*A(2,1:N)+A(3,1:N)*A(3,1:N))
-  cart2sphere1(2,:) = acos(A(3,:)/B(1,:))
-  cart2sphere1(3,:) = atan(A(2,:)/A(1,:))
-end function  cart2sphere1
+  real (c_float), intent(OUT):: B(3,N)
+  B(1,:) = sqrt(A(1,1:N)*A(1,1:N)+A(2,1:N)*A(2,1:N)+A(3,1:N)*A(3,1:N))
+  B(2,:) = acos(A(3,:)/B(1,:))
+  B(3,:) = atan(A(2,:)/A(1,:))
+end subroutine cart2sphere1
 
   ! subroutine cart2sphere2(N,A,B) bind (c,name='cart2sphere2')
   !   use iso_c_binding
