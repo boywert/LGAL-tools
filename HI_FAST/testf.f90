33596bdb8cf3ece0861086224faef63d2e92c43b
Chaichalit Srisawat <boywert@gmail.com>
2017-07-06 17:57:49 +0700

    quick commit

--- HI_FAST/testf.f90
+++ HI_FAST/testf.f90
@@ -51,9 +51,9 @@ subroutine make_sphere(N,boxsize,A,B) bind (c,name='make_sphere')
            AC(1,:) = A(1,:) - (i-1)*boxsize
            AC(2,:) = A(2,:) - (j-1)*boxsize
            AC(3,:) = A(3,:) - (k-1)*boxsize
-           B(1,index*N+1:index*N+N) = (AC(1,1:N)*AC(1,1:N)+AC(2,1:N)*AC(2,1:N)+AC(3,1:N)*AC(3,1:N))
-           B(2,index*N+1:index*N+N) = acos(AC(3,:)/B(1,:))
-           B(3,index*N+1:index*N+N) = atan(AC(2,:)/AC(1,:))
+           B(1,index*N+1:index*N+N) = (A(1,1:N)*A(1,1:N)+A(2,1:N)*A(2,1:N)+A(3,1:N)*A(3,1:N))
+           B(2,index*N+1:index*N+N) = acos(A(3,:)/B(1,:))
+           B(3,index*N+1:index*N+N) = atan(A(2,:)/A(1,:))
            print *, "finish",index
         end do
      end do
