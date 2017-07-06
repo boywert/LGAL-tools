d25ad0d465b5a23a84976a925098edf40809478d
Chaichalit Srisawat <boywert@gmail.com>
2017-07-06 20:31:12 +0700

    quick commit

--- HI_FAST/test_light.py
+++ HI_FAST/test_light.py
@@ -104,7 +104,7 @@ def plot_coldgas(z):
             zz = "%10.3f"%(z)
         file_prefix = "model_z"+zz.strip()
         if not index in gal:
-            (nTrees[index],nGals[index],nTreeGals[index],gal[index]) = read_lgal.readsnap_lgal_advance(model_paths[i],file_prefix,0,5,filter[i],dt[i],1)
+            (nTrees[index],nGals[index],nTreeGals[index],gal[index]) = read_lgal.readsnap_lgal_advance(model_paths[i],file_prefix,0,511,filter[i],dt[i],1)
         
     
         R = numpy.empty(nGals[index]*8,dtype=c_float)
