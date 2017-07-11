fcf4126e480606ef1cd27aedf0500a7c42d86631
Chaichalit Srisawat <boywert@gmail.com>
2017-07-11 10:33:40 +0700

    quick commit

---
+++ HI_FAST/test_read.py
@@ -0,0 +1,109 @@
+from mass_fn import *
+from globalconf import *
+from math import *
+import matplotlib
+matplotlib.use('Agg') 
+import pylab
+import sys
+import numpy
+import os
+import matplotlib.pyplot as plt
+os.system("cp dummy_dtype.py LGalaxyStruct.py")
+import LGalaxyStruct
+import add_observations
+sys.path.append("../python/")
+import read_lgal_advance as read_lgal
+import timeit
+from numpy.ctypeslib import ndpointer
+from ctypes import CDLL, POINTER, c_int, c_float, c_double
+#import test as mymodule
+mymodule = CDLL('./test.so')
+_twodimp = ndpointer(dtype=c_float,ndim=2)
+arg2 = ndpointer(ndim=2)
+arg3 = ndpointer(shape=(10,10))
+mymodule.make_sphere.argtypes = [c_int, c_float, _twodimp, _twodimp]
+import healpy
+from timeit import default_timer as timer
+rank = "0"
+os.system("mkdir -p ../tmp/"+rank)
+def loadfilter(structfile):
+    sys.path.insert(0,"../tmp/"+rank)
+    os.system("cp "+structfile+" ../tmp/"+rank+"/LGalaxyStruct.py")
+    os.system("rm -f ../tmp/"+rank+"/LGalaxyStruct.pyc")
+    reload(LGalaxyStruct)
+    filter = LGalaxyStruct.properties_used
+    for fi in filter:
+        fi = False    
+    filter['Pos'] = True
+    filter['Vel'] = True
+    
+    dt = LGalaxyStruct.struct_dtype
+    return (filter,dt)
+
+dt = []
+filter = []
+for i in range(len(struct_file)):
+    (f,t) = loadfilter(struct_file[i])
+    filter.append(f)
+    dt.append(t)
+
+#filter model
+filter_tmp = []
+dt_tmp = []
+model_names_tmp = []
+struct_file_tmp = []
+model_labels_tmp = []
+model_paths_tmp = []
+for i in range(len(use_model)):
+    if use_model[i]:
+        filter_tmp.append(filter[i])
+        dt_tmp.append(dt[i])
+        model_names_tmp.append(model_names[i])
+        struct_file_tmp.append(struct_file[i])
+        model_labels_tmp.append(model_labels[i])
+        model_paths_tmp.append(model_paths[i])
+
+filter = filter_tmp
+dt = dt_tmp
+model_names = model_names_tmp
+struct_file = struct_file_tmp
+model_labels = model_labels_tmp
+model_paths = model_paths_tmp       
+
+
+
+pylab.rc('text', usetex=True)
+pylab.rc('lines', linewidth=2)
+plt.rcParams['ytick.major.size'] = 8
+plt.rcParams['xtick.major.size'] = 8
+#zlist = open(zlistfile,"r").readlines()
+
+NSIDE = 2048
+
+def readgal(z):
+    #firstfile = 0
+    #lastfile = 127
+    config = {}
+
+    try:
+        gal
+    except NameError:
+        gal = {}
+        nTrees = {}
+        nGals = {}
+        nTreeGals = {}
+
+    r = {}
+    theta = {}
+    phi = {}
+    for i in range(len(model_names)):
+        index = model_names[i]
+        if index[:4] == "lgal":
+            zz = "%10.2f"%(z)
+        elif index[:4] == "sage":
+            zz = "%10.3f"%(z)
+        file_prefix = "model_z"+zz.strip()
+        if not index in gal:
+            (nTrees[index],nGals[index],nTreeGals[index],gal[index]) = read_lgal.readsnap_lgal_advance(model_paths[i],file_prefix,0,511,filter[i],dt[i],1)
+            return gal[index]
+        
