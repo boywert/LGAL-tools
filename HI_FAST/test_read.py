f5e148f6db7f1322e34119d27bc0b0ebd8eea490
Chaichalit Srisawat <boywert@gmail.com>
2017-07-16 15:27:03 +0700

    quick commit

--- HI_FAST/test_read.py
+++ HI_FAST/test_read.py
@@ -161,7 +161,7 @@ def main():
     alist.sort()
     alist = alist[::-1]
     print alist,a_from_z(last_z),a_from_z(first_z)
-    return
+
     fc_list = numpy.arange(nu_from_z(first_z),nu_from_z(last_z)-f_step,-1*f_step)
     fb_list = numpy.empty(len(fc_list)-1,dtype = numpy.float32)
     for i in range(len(fc_list)-1):
