5b95d0ec74da83822d4bb31c83298eec131f75ce
Chaichalit Srisawat <boywert@gmail.com>
2017-07-16 19:49:24 +0700

    quick commit

--- HI_FAST/test_read.py
+++ HI_FAST/test_read.py
@@ -171,7 +171,7 @@ def main():
     start_r = 0.0
     for i in range(len(alist)-1):
         z = "%10.3f" % (z_from_a(alist[i]))
-        alist_distance = cosmo.comoving_distance(z_from_a(alist[i+1])).value*0.73
+        alist_distance = min(cosmo.comoving_distance(z_from_a(alist[i+1]).value,cosmo.comoving_distance(first_z).value)*0.73
         ngal_i,gal_i,pos_i,vR_i = readgal(float(z))
         gallist = numpy.where((pos_i[:,0] >= start_r) & (pos_i[:,0] <= alist_distance))[0]
         print "z = ",z,"a=",alist[i],"r = ",start_r,"-",alist_distance
