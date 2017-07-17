f2844fb8a1a3eb67c7778d8482ca720a57eee35a
Chaichalit Srisawat <boywert@gmail.com>
2017-07-17 08:19:22 +0700

    quick commit

--- HI_FAST/test_read.py
+++ HI_FAST/test_read.py
@@ -211,6 +211,7 @@ def main():
         ogal['PosR'] = pos_i[gallist,0]
         ogal['PosTheta'] = pos_i[gallist,1]
         ogal['PosPhi'] = pos_i[gallist,2]
+        print ogal[(ogal['PosTheta'] < 0.) | (ogal['PosTheta'] > numpy.pi)] 
         ogal['Healpix'] = healpy.pixelfunc.ang2pix(NSIDE, ogal['PosTheta'][:], ogal['PosPhi'][:])
         ogal['VelR'] = vR_i[gallist,0]
         ogal['VelTheta'] = vR_i[gallist,1]
