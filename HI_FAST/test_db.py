b7342856c4ad2bac0047a86d8213bdb77576feca
Chaichalit Srisawat <boywert@gmail.com>
2017-09-15 02:40:54 +0700

    quick commit

--- HI_FAST/test_db.py
+++ HI_FAST/test_db.py
@@ -31,25 +31,28 @@ from timeit import default_timer as timer
 rank = "0"
 os.system("mkdir -p ../tmp/"+rank)
 db_struct = numpy.dtype([
-('PosX'                      , numpy.float32),
-('PosY'                      , numpy.float32),
-('PosZ'                      , numpy.float32),
-('PosR'                      , numpy.float32),
-('PosTheta'                  , numpy.float32),
-('PosPhi'                    , numpy.float32),
-('VelX'                      , numpy.float32),
-('VelY'                      , numpy.float32),
-('VelZ'                      , numpy.float32),
-('VelR'                      , numpy.float32),
-('VelTheta'                  , numpy.float32),
-('VelPhi'                    , numpy.float32),
-('StellarMass'               , numpy.float32),
-('ColdGas'                   , numpy.float32), 
-('Healpix'                   , numpy.int32),
-('Frequency'                 , numpy.float32),
-('LuminosityDistance'        , numpy.float32),
-('NeutralH'                  , numpy.float32),
-('Intensity'                 , numpy.float32)])
+    ('PosX'                      , numpy.float32),
+    ('PosY'                      , numpy.float32),
+    ('PosZ'                      , numpy.float32),
+    ('PosR'                      , numpy.float32),
+    ('PosTheta'                  , numpy.float32),
+    ('PosPhi'                    , numpy.float32),
+    ('VelX'                      , numpy.float32),
+    ('VelY'                      , numpy.float32),
+    ('VelZ'                      , numpy.float32),
+    ('VelR'                      , numpy.float32),
+    ('VelTheta'                  , numpy.float32),
+    ('VelPhi'                    , numpy.float32),
+    ('StellarMass'               , numpy.float32),
+    ('ColdGas'                   , numpy.float32), 
+    ('Healpix'                   , numpy.int32),
+    ('Frequency'                 , numpy.float32),
+    ('LuminosityDistance'        , numpy.float32),
+    ('Redshift'                  , numpy.float32),
+    ('NeutralH'                  , numpy.float32),
+    ('DeltaFrequency'            , numpy.float32),
+    ('FluxDensity'               , numpy.float32),
+    ('Flux'                 , numpy.float32)])
 
 def loadfilter(structfile):
     sys.path.insert(0,"../tmp/"+rank)
@@ -169,7 +172,8 @@ def main():
     StellarMass real, ColdGas real,
     Healpix int,
     Frequency real,
-    LuminosityDistance real, NeutralH real, Intensity real)''')
+    LuminosityDistance real, Redshift real, NeutralH real, 
+    DeltaFrequency real, FluxDensity real, Flux real)''')
     
     for i in range(len(model_names)):
         for file in range(512):
